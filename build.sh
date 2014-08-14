405c208777ec694fabdc84bcf346305428d4ce14
geraldss <gerald@couchbase.com>
2014-08-14 11:45:52 -0700

    Tweaked build messages.

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
+
 echo cd parser/n1ql
 cd parser/n1ql
-./build.sh
+./build.sh $1
 cd ../..
 
 echo cd server/main
 cd server/main
-./build.sh
+./build.sh $1
 cd ../..
 
 echo cd shell
 cd shell
-./build.sh
+./build.sh $1
 cd ..
