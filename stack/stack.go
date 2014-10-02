b9b88de692da5aceaa30f279f59487946c9f20ee
manik <manik@couchbase.com>
2014-10-02 22:17:08 +0530

    Checkin missing stack impl

---
+++ stack/stack.go
@@ -0,0 +1,43 @@
+//  Copyright (c) 2013 Couchbase, Inc.
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software distributed under the
+//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+//  either express or implied. See the License for the specific language governing permissions
+//  and limitations under the License.
+
+package stack
+
+import ()
+
+type Stack struct {
+	top  *Element
+	size int
+}
+
+type Element struct {
+	value interface{}
+	next  *Element
+}
+
+func (s *Stack) Size() int {
+	return s.size
+}
+
+// Push a new element onto the stack
+func (s *Stack) Push(value interface{}) {
+	s.top = &Element{value, s.top}
+	s.size++
+}
+
+// Remove the top element from the stack and return it's value
+// If the stack is empty, return nil
+func (s *Stack) Pop() (value interface{}) {
+	if s.size > 0 {
+		value, s.top = s.top.value, s.top.next
+		s.size--
+		return
+	}
+	return nil
+}
---
+++ stack/stack_test.go
@@ -0,0 +1,36 @@
+//  Copyright (c) 2013 Couchbase, Inc.
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software distributed under the
+//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+//  either express or implied. See the License for the specific language governing permissions
+//  and limitations under the License.
+
+package stack
+
+import (
+	"testing"
+)
+
+func TestStack(t *testing.T) {
+	s := Stack{}
+
+	s.Push(1)
+	s.Push(2)
+
+	v2 := s.Pop()
+	if v2 != 2 {
+		t.Errorf("Expected 2, got %v", v2)
+	}
+
+	v1 := s.Pop()
+	if v1 != 1 {
+		t.Errorf("Expected 1, got %v", v1)
+	}
+
+	v0 := s.Pop()
+	if v0 != nil {
+		t.Errorf("Expected nil, got %v", v0)
+	}
+}
