08b8472856942d403f2c696238a7d0bb4e9e7487
IshaK <isha@couchbase.com>
2014-11-13 12:22:40 -0800

    Update documentation for each method implementation in value

--- value/missing.go
+++ value/missing.go
@@ -13,21 +13,35 @@ import (
 	"fmt"
 )
 
-// Missing value
+/*
+Missing value is of type string
+*/
 type missingValue string
 
+/*
+Initialized to an empty string cast to missingValue.
+*/
 var MISSING_VALUE Value = missingValue("")
 
+/*
+Returns variable MISING_VALUE.
+*/
 func NewMissingValue() Value {
 	return MISSING_VALUE
 }
 
-// Ideally, we should never marshal a MISSING value.
+/*
+Ideally, we should never marshal a MISSING value. Hence
+_NULL_BYTES is returned.
+*/
 func (this missingValue) MarshalJSON() ([]byte, error) {
 	return _NULL_BYTES, nil
 }
 
-// Description of which property or index was undefined (if known).
+/*
+Description of which property or index was undefined (if known).
+If not known, return a message stating Missing field or index.
+*/
 func (this missingValue) Error() string {
 	if string(this) != "" {
 		return fmt.Sprintf("Missing field or index %s.", string(this))
@@ -36,72 +50,129 @@ func (this missingValue) Error() string {
 	}
 }
 
+/*
+Type MISSING
+*/
 func (this missingValue) Type() Type { return MISSING }
 
+/*
+Returns nil, since this is not a valid Go type.
+*/
 func (this missingValue) Actual() interface{} {
 	return nil
 }
 
+/*
+Returns true if other is MISSING. As per N1QL specs. 
+*/
 func (this missingValue) Equals(other Value) bool {
 	return other.Type() == MISSING
 }
 
+/*
+Returns an integer representing the position of Missing with 
+respect to the other values type by subtracting them and 
+casting the result to an integer. 
+*/
 func (this missingValue) Collate(other Value) int {
 	return int(MISSING - other.Type())
 }
 
+/*
+As per the N1ql specs the truth-value of a missing evaluates 
+to a false, and hence the Truth method returns a false.
+*/
 func (this missingValue) Truth() bool {
 	return false
 }
 
+/*
+Return receiver this.
+*/
 func (this missingValue) Copy() Value {
 	return this
 }
 
+/*
+Return receiver.
+*/
 func (this missingValue) CopyForUpdate() Value {
 	return this
 }
 
+/*
+Calls missingField.
+*/
 func (this missingValue) Field(field string) (Value, bool) {
 	return missingField(field), false
 }
 
+/*
+Invalid for missing.
+*/
 func (this missingValue) SetField(field string, val interface{}) error {
 	return Unsettable(field)
 }
 
+/*
+Invalid for missing.
+*/
 func (this missingValue) UnsetField(field string) error {
 	return Unsettable(field)
 }
 
+/*
+Calls missingIndex.
+*/
 func (this missingValue) Index(index int) (Value, bool) {
 	return missingIndex(index), false
 }
 
+/*
+Invalid for missing.
+*/
 func (this missingValue) SetIndex(index int, val interface{}) error {
 	return Unsettable(index)
 }
 
+/*
+Returns MISSING_VALUE.
+*/
 func (this missingValue) Slice(start, end int) (Value, bool) {
 	return MISSING_VALUE, false
 }
 
+/*
+Returns MISSING_VALUE.
+*/
 func (this missingValue) SliceTail(start int) (Value, bool) {
 	return MISSING_VALUE, false
 }
 
+/*
+Returns the input buffer as is.
+*/
 func (this missingValue) Descendants(buffer []interface{}) []interface{} {
 	return buffer
 }
 
+/*
+Missing has no fields to list. Hence return nil.
+*/
 func (this missingValue) Fields() map[string]interface{} {
 	return nil
 }
 
+/*
+Cast input field to missingValue.
+*/
 func missingField(field string) missingValue {
 	return missingValue(field)
 }
 
+/*
+Cast input index to missingValue after casting to string.
+*/
 func missingIndex(index int) missingValue {
 	return missingValue(string(index))
 }
--- value/value.go
+++ value/value.go
@@ -8,11 +8,9 @@
 //  and limitations under the License.
 
 /*
-
 Package value represents the N1QL data model. It is the in memory
 representation of the data in flight. It provides a native abstraction
 for JSON data values, with delayed parsing.
-
 */
 package value
 
@@ -32,7 +30,6 @@ const (
 )
 
 /*
-
 Function ToTristate converts a boolean into a Tristate type. If the
 function argument represents a True then it returns a True Tristate
 value, else it returns False. It is used to represent the metrics
@@ -40,7 +37,6 @@ value, else it returns False. It is used to represent the metrics
 BaseRequest) in server/http/http_request.go, which handles the http
 request step in the N1ql Architecture diagram and provides the
 metadata before the results.
-
 */
 func ToTristate(b bool) Tristate {
 	if b {
@@ -51,24 +47,22 @@ func ToTristate(b bool) Tristate {
 }
 
 /*
-
 Function ToBool converts a Tristate value to a boolean.
-
 */
 func ToBool(t Tristate) bool {
 	return t == TRUE
 }
 
-// The data types supported by Value, present and supported in N1QL.
+/*
+The data types supported by Value, present and supported in N1QL.
+*/
 type Type int
 
 /*
-
 List of valid N1QL types. Missing is specific to N1QL and Binary
 refers to unparsed JSON bytes, represented by a bytes array. It is a
 non-JSON value. The value type JSON is all-encompassing and covers all
 N1ql values.
-
 */
 const (
 	MISSING = Type(iota) // Missing field
@@ -82,18 +76,18 @@ const (
 	JSON                 // Non-specific JSON; used in result sets
 )
 
-// Stringer interface, which is used in expression/stringer.go to
-// visit nodes and convert from type defined to a string and return
-// it.
+/*
+Stringer interface, which is used in expression/stringer.go to
+visit nodes and convert from type defined to a string and return
+it.
+*/
 func (this Type) String() string {
 	return _TYPE_NAMES[this]
 }
 
 /*
-
 The _TYPE_NAMES variable is a slice of strings that contains the Type
 and its corresponding string representation.
-
 */
 var _TYPE_NAMES = []string{
 	MISSING: "missing",
@@ -107,11 +101,15 @@ var _TYPE_NAMES = []string{
 	JSON:    "json",
 }
 
-// When you try to set a nested property or index that does not exist,
-// the return error will be Unsettable.
+/*
+When you try to set a nested property or index that does not exist,
+the return error will be Unsettable.
+*/
 type Unsettable string
 
-// Description of which property or index was unsettable (if known).
+/*
+Description of which property or index was unsettable (if known).
+*/
 func (this Unsettable) Error() string {
 	if string(this) != "" {
 		return fmt.Sprintf("Field or index %s is not settable.", string(this))
@@ -120,189 +118,180 @@ func (this Unsettable) Error() string {
 }
 
 /*
-
 The _MARSHAL_ERROR constant represents an error string that is output
 when there is an unexpected marshal error on valid data. Marshal
 returns the JSON encoding of any input interface. It is used while
 implementing the method MarshalJSON
-
 */
 const _MARSHAL_ERROR = "Unexpected marshal error on valid data."
 
-// A channel of Value objects
+/*
+A channel of Value objects
+*/
 type ValueChannel chan Value
 
-// Value collections
+/* 
+Value collections
+*/
 type Values []Value
 type CompositeValues []Values
 
-// An interface for storing and manipulating a JSON value.Each 'value'
-// implements the methods that correspond to it.
+/* 
+An interface for storing and manipulating a JSON value.Each 'value'
+implements the methods that correspond to it.
+*/
 type Value interface {
 	/* 
-
            This method is used by the json package. It is used to
            convert to JSON byte encoding; and returns a byte array of
            valid JSON values. error is always nil.
-
         */
         MarshalJSON() ([]byte, error)                  
 
         /*
-
            Returns the type of the input based on the previously
            defined Types(Data type constant).
-
         */
 	Type() Type                                     
 
         /*
-
            N1QL to native Go representation of method receiver. It
            returns an interface.
-
         */
 	Actual() interface{}                            
 
         /*
-
            Returns a Boolean based on if receiver and input argument
            Value are equal. It is faster than Collate().
-
         */
 	Equals(other Value) bool                        
 
         /*
-
            Returns –int, 0 or +int depending on if the receiver this
            sorts less than, equal to, or greater than the input
            argument Value to the method. It uses the type order
            defined previously.  (This order has also been defined in
            the N1QL spec under order by.)
-
         */
 	Collate(other Value) int                        
-
+  
         /*
-
            Returns the Boolean interpretation of the input this for
            different values(Truth value).
-
         */
 	Truth() bool                                    
 
         /*
-
            Returns a Value, which is a shallow copy of the input.
-
         */
 	Copy() Value                                    
 
         /*
-
            Returns a Value that is a deep copy of the receiver. It is
            used for Updates.
-
         */
 	CopyForUpdate() Value                          
 
         /*
-
            Access a field or nested data in an object.(Object field
            dereference) Returns a value and a Boolean; the value being
            either a missing or the N1QL Value of the input for
            objects, and a true if found.  This function returns a
            missingField and false; for all the value types except
            Object.
-
         */
 	Field(field string) (Value, bool)               
 
         /*
-
            Set a field in an object. For types other than object,
            Unsettable is called since this method is not valid for
            those types.
-
         */
 	SetField(field string, val interface{}) error   
 
         /*
-
            It deletes the input field for an object. For types other
            than object, Unsettable is called.
-
         */
 	UnsetField(field string) error                  
 
         /*
-
            Access an entry at a particular index in the array.(Array
            index dereference) The return value is the Value at that
            index and a Boolean; the value being a N1QL value of the
            input for the slice and a true if found. It returns
            missingIndex and false; for all types except slice/array.
-
         */
 	Index(index int) (Value, bool)                  
 	
         /*
-
            Populate the value at a particular index in the slice with
            val of type interface. Slices do not extend beyond their
            length. For any attempt to set an index that is greater
            than length, Unsettable is called.
-
         */
         SetIndex(index int, val interface{}) error      
 
         /*
-
            Array slicing. Takes a start and end index and returns a
            new slice; also returns a bool that is true if receiver is
            of type array. For all non slice values it returns
            NULL_VALUE and false.
-
         */
 	Slice(start, end int) (Value, bool)             
  
         /*
-
            Array slicing to the end of the array. Takes a start index
            and returns a new slice till the end of the slice; bool
            returns true if found. For all non array/slice values it
            returns a NULL_VALUE and false.
-
         */
 	SliceTail(start int) (Value, bool)              
 
         /*
-
            Lists the descendants of an array or object in depth first
            order (multilevel list flattening) by adding it to an input
            buffer and returning it.
-
         */
 	Descendants(buffer []interface{}) []interface{} 
 	
         /*
-
            Gives you the object fields by adding them to a string to
            interface map. This returns null for all types except
            object. ( it is used in expression/nav_field.go to navigate
            through fields of value Value).
-
         */
         Fields() map[string]interface{}                
 }
 
+/*
+This is defined as a slice of reflect Types and has 3 pre-defined 
+types (reflect.TypeOf returns the type of the input argument, string,
+boolean, and number.
+*/
 var _CONVERSIONS = []reflect.Type{
 	reflect.TypeOf(0.0),
 	reflect.TypeOf(false),
 	reflect.TypeOf(""),
 }
 
-// Bring a data object into the Value type system
+/*
+Bring a data object into the Value type system from a Go Type. If the 
+input value is nil then we return a NULL_VALUE, and if it is already 
+a valid N1QL value then we return it as is. For a float64, string and 
+Bool type for val we cast it to the valid N1QL type and return it 
+(float is defined as float64 at this time). For a slice of interfaces, 
+we return a value of type sliceValue. For a map from string to interface 
+it returns an objectValue. For a slice of Value, a slice of interfaces 
+of length val is created, and on ranging over the values and we add 
+them to it. Finally we cast it to a sliceValue and return it. In the 
+event it’s a slice of AnnotatedValue the procedure followed is the same 
+as that for a slice of value. If the input type is a slice of bytes then 
+we call the function NewValueFromBytes on the value. The default 
+behavior is to range over the predefined _Conversions, and check if 
+it can be converted to one of these predecfined types. If so, we create
+a NewValue and if not return an error.
+*/
 func NewValue(val interface{}) Value {
 	if val == nil {
 		return NULL_VALUE
@@ -348,7 +337,21 @@ func NewValue(val interface{}) Value {
 	}
 }
 
-// Create a new Value from a slice of bytes. (this need not be valid JSON)
+/*
+Create a new Value from a slice of bytes. (this need not be valid JSON)
+We first validate the input bytes to check if it is valid json encoded 
+data using a function, defined in the public repository dustin/gojson. 
+It returns a nil if the json is valid. Try to identify the type of json 
+value it is by calling identifyType. If a Number, string, Boolean or 
+null value we call Unmarshal to parse the json encoded data and store 
+result.  If an error was returned by the Unmarshal function, the data 
+was not valid json encoding. In the case it is of object or array, 
+create and initialize a new variable as a pointer to the structure 
+parsedValue, and assign raw to the bytes. In the event an err was 
+returned by the validate function, we set the parsedType variable in 
+the struct to BINARY. If there was no error we set it to the type we 
+identified before and return the pointer to the struct as a Value.
+*/
 func newValueFromBytes(bytes []byte) Value {
 	var parsedType Type
 	err := json.Validate(bytes)
@@ -381,16 +384,37 @@ func newValueFromBytes(bytes []byte) Value {
 	return rv
 }
 
+/*
+Function takes an input interface and returns an interface.
+*/
 type copyFunc func(interface{}) interface{}
 
+/*
+It is passed an interface and returns itself. It allows us to 
+access the input object.
+*/
 func self(val interface{}) interface{} {
 	return val
 }
 
+/*
+Calls the CopyForUpdate method for that particular value type 
+and returns the interface returned by that call. This is used 
+by array.go, when there is a necessity to copy all the elements 
+of the array, which can have any JSON type.
+*/
 func copyForUpdate(val interface{}) interface{} {
 	return NewValue(val).CopyForUpdate()
 }
 
+/*
+Used to return the type of input bytes. It ranges over bytes, 
+and classifies it into an object (if '{' is seen), array ('['), 
+string ('"'), number (for any digit and '-'), boolean ('t/f'),
+and null ('n'). If a whitespace is encountered, look at the 
+next byte. If none of these types fit then we throw an error 
+stating that we were unable to identify they type of JSON data.
+*/
 func identifyType(bytes []byte) Type {
 	for _, b := range bytes {
 		switch b {
