ace4f95f018b76f933599a5e947cfc25cde575be
IshaK <isha@couchbase.com>
2014-12-03 09:08:51 -0800

    Update doc for coll expr for in,within and exists

--- expression/coll_exists.go
+++ expression/coll_exists.go
@@ -13,10 +13,19 @@ import (
 	"github.com/couchbaselabs/query/value"
 )
 
+/*
+Represents the Collection expression Exists. Type Exists
+is a struct that implements UnaryFuncitonBase.
+*/
 type Exists struct {
 	UnaryFunctionBase
 }
 
+/*
+The function NewExists calls NewUnaryFunctionBase
+to define exists collection expression with input operand
+expression as input.
+*/
 func NewExists(operand Expression) *Exists {
 	rv := &Exists{
 		*NewUnaryFunctionBase("exists", operand),
@@ -26,16 +35,32 @@ func NewExists(operand Expression) *Exists {
 	return rv
 }
 
+/*
+It calls the VisitExists method by passing in the receiver to
+and returns the interface. It is a visitor pattern.
+*/
 func (this *Exists) Accept(visitor Visitor) (interface{}, error) {
 	return visitor.VisitExists(this)
 }
 
+/*
+It returns a value type BOOLEAN.
+*/
 func (this *Exists) Type() value.Type { return value.BOOLEAN }
 
+/*
+Calls the Eval method for Unary functions and passes in the
+receiver, current item and current context.
+*/
 func (this *Exists) Evaluate(item value.Value, context Context) (value.Value, error) {
 	return this.UnaryEval(this, item, context)
 }
 
+/*
+This method returns true if he value is an array and contains at least one element.
+This is done by checking the length of the array. If the type of input value
+is missing then return a missing value, and for all other types return null.
+*/
 func (this *Exists) Apply(context Context, arg value.Value) (value.Value, error) {
 	if arg.Type() == value.ARRAY {
 		a := arg.Actual().([]interface{})
@@ -47,6 +72,10 @@ func (this *Exists) Apply(context Context, arg value.Value) (value.Value, error)
 	}
 }
 
+/*
+The constructor returns a NewExists with the operand
+cast to a Function as the FunctionConstructor.
+*/
 func (this *Exists) Constructor() FunctionConstructor {
 	return func(operands ...Expression) Function {
 		return NewExists(operands[0])
--- expression/coll_within.go
+++ expression/coll_within.go
@@ -13,10 +13,19 @@ import (
 	"github.com/couchbaselabs/query/value"
 )
 
+/*
+Represents the Collection expression Within. Type Within is a
+struct that implements BinaryFunctionBase.
+*/
 type Within struct {
 	BinaryFunctionBase
 }
 
+/*
+The function NewWithin calls NewBinaryFunctionBase
+to define In collection expression with input operand
+expressions first and second, as input.
+*/
 func NewWithin(first, second Expression) Function {
 	rv := &Within{
 		*NewBinaryFunctionBase("within", first, second),
@@ -26,16 +35,37 @@ func NewWithin(first, second Expression) Function {
 	return rv
 }
 
+/*
+It calls the VisitWithin method by passing in the receiver to
+and returns the interface. It is a visitor pattern.
+*/
 func (this *Within) Accept(visitor Visitor) (interface{}, error) {
 	return visitor.VisitWithin(this)
 }
 
+/*
+It returns a value type BOOLEAN.
+*/
 func (this *Within) Type() value.Type { return value.BOOLEAN }
 
+/*
+Calls the Eval method for Binary functions and passes in the
+receiver, current item and current context.
+*/
 func (this *Within) Evaluate(item value.Value, context Context) (value.Value, error) {
 	return this.BinaryEval(this, item, context)
 }
 
+/*
+WITHIN evaluates to TRUE if the right-hand-side first value contains
+the left-hand-side second value (or name and value) as a child or
+descendant (i.e. directly or indirectly). If either
+of the input operands are missing, return missing value, and if
+the first operand is not an array and the second is not an object
+return a null value. Range through the descendants of the second
+value object, and check if the first value is equal to each
+descendant; return true. For all other cases return false.
+*/
 func (this *Within) Apply(context Context, first, second value.Value) (value.Value, error) {
 	if first.Type() == value.MISSING || second.Type() == value.MISSING {
 		return value.MISSING_VALUE, nil
@@ -53,12 +83,22 @@ func (this *Within) Apply(context Context, first, second value.Value) (value.Val
 	return value.FALSE_VALUE, nil
 }
 
+/*
+The constructor returns a NewWithin with the operands
+cast to a Function as the FunctionConstructor.
+*/
 func (this *Within) Constructor() FunctionConstructor {
 	return func(operands ...Expression) Function {
 		return NewWithin(operands[0], operands[1])
 	}
 }
 
+/*
+This function implements the not Within collection operation.
+It calls the NewNot over the NewWithin to return an expression that
+is a complement of its boolean return type.
+(NewNot represents the Not logical operation)
+*/
 func NewNotWithin(first, second Expression) Expression {
 	return NewNot(NewWithin(first, second))
 }
