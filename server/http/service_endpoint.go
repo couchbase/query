18d4334b7db85948f34fb1c637e2db59d4cc9da8
Colm McHugh <colm@couchbase.com>
2015-07-29 13:38:55 +0100

    Do not accept RC4 ciphers. MB-15841.
    Change-Id: Ie7437378b567200ba7025b370382c997df95a809
    Reviewed-on: http://review.couchbase.org/53859
    Reviewed-by: Colm McHugh <colm@couchbase.com>
    Tested-by: Colm McHugh <colm@couchbase.com>

--- server/http/service_endpoint.go
+++ server/http/service_endpoint.go
@@ -80,6 +80,16 @@ func (this *HttpEndpoint) ListenTLS() error {
 			Certificates: []tls.Certificate{tlsCert},
 			ClientAuth:   tls.NoClientCert,
 			MinVersion:   tls.VersionTLS10,
+			CipherSuites: []uint16{tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256},
 		}
 		tls_ln := tls.NewListener(ln, cfg)
 		this.listenerTLS = tls_ln
