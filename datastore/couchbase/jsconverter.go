5f314506f06a15a60a79518ddf2a9f75d8dae28a
manik <manik.taneja@gmail.com>
2015-11-20 17:24:02 +0530

    Allow n1ql to use existing views by specifying the view name in the WITH clause while creating a view index create index idx on bucket(fieldname) using view with "viewidx" With above create index statement, n1ql will create a view index using the design document from an existing view "viewidx" This change also fixes some a bug where the create view index failed when meta() function was used in the where clause Change-Id: Ia8993a1ab48fc04e830f03296fa53e8e1d3a56ef Reviewed-on: http://review.couchbase.org/57174 Reviewed-by: Gerald Sangudi <gerald@couchbase.com> Reviewed-by: Manik Taneja <manik@couchbase.com> Tested-by: Manik Taneja <manik@couchbase.com> Reviewed-on: http://review.couchbase.org/57923

--- datastore/couchbase/jsconverter.go
+++ datastore/couchbase/jsconverter.go
@@ -536,7 +536,6 @@ func (this *JSConverter) VisitFunction(expr expression.Function) (interface{}, e
 		buf.WriteString("Date.now().toString()")
 	case "meta":
 		buf.WriteString("meta")
-		buf.WriteString(".")
 		nobracket = true
 	default:
 		nopush = true
