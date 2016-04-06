c5036ca8a8c48af4d1ebbe4a7a21f8722e80d506
Marco Greco <marco.greco@couchbase.com>
2016-04-06 14:26:10 +0100

    MB-19068  only build the lexer when required
    Change-Id: I2b5dad0acb60a3093ca1d7266e470c540c9dadf6
    Reviewed-on: http://review.couchbase.org/62490
    Reviewed-by: Isha Kandaswamy <isha@couchbase.com>
    Tested-by: Marco Greco <marco.greco@couchbase.com>

--- parser/n1ql/build.sh
+++ parser/n1ql/build.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
-echo nex n1ql.nex
-nex n1ql.nex
+if [ n1ql.nex -nt n1ql.nn.go ]
+then 
+    echo nex n1ql.nex
+    nex n1ql.nex
+    go fmt n1ql.nn.go
+fi
 echo go tool yacc n1ql.y
 go tool yacc n1ql.y
 echo go build
