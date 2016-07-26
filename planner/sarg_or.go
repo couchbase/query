a148e74ac4c215702e39bf567f5478f3948676c7
sitaramv <sitaram.vemulapalli@couchbase.com>
2016-07-25 21:34:11 -0700

    MB-18482 Improve OR span
    Change-Id: I06da0e0c15137c86ff1f5e6f9ec3a1c200791bdd
    Reviewed-on: http://review.couchbase.org/66189
    Reviewed-by: Gerald Sangudi <gerald@couchbase.com>
    Tested-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>

--- planner/sarg_or.go
+++ planner/sarg_or.go
@@ -27,15 +27,35 @@ func newSargOr(pred *expression.Or) *sargOr {
 
 		spans := make(plan.Spans, 0, len(pred.Operands()))
 		emptySpan := false
-		fullSpans := false
+		valuedSpan := false
+		exactValuedSpan := false
+		nullSpan := false
+		fullSpan := false
 		for _, child := range pred.Operands() {
+
 			cspans, err := sargFor(child, expr2, rv.MissingHigh())
 			if err != nil {
 				return nil, err
 			}
 
 			if len(cspans) == 0 {
-				return nil, err
+				return cspans, nil
+			}
+
+			if cspans[0] == _EXACT_FULL_SPANS[0] {
+				return cspans, nil
+			}
+
+			if cspans[0] == _FULL_SPANS[0] {
+				fullSpan = true
+			}
+
+			if cspans[0] == _VALUED_SPANS[0] {
+				valuedSpan = true
+			}
+
+			if cspans[0] == _EXACT_VALUED_SPANS[0] {
+				exactValuedSpan = true
 			}
 
 			if cspans[0] == _EMPTY_SPANS[0] {
@@ -43,22 +63,22 @@ func newSargOr(pred *expression.Or) *sargOr {
 				continue
 			}
 
-			if cspans[0] == _EXACT_FULL_SPANS[0] {
-				return _EXACT_FULL_SPANS, nil
+			if cspans[0] == _NULL_SPANS[0] {
+				nullSpan = true
 			}
 
 			if len(spans)+len(cspans) > _FULL_SPAN_FANOUT {
 				return _FULL_SPANS, nil
 			}
 
-			if cspans[0] == _FULL_SPANS[0] {
-				fullSpans = true
-			}
-
 			spans = append(spans, cspans...)
 		}
 
-		if fullSpans {
+		if exactValuedSpan && nullSpan {
+			return _EXACT_FULL_SPANS, nil
+		}
+
+		if (valuedSpan && nullSpan) || fullSpan {
 			return _FULL_SPANS, nil
 		}
 
