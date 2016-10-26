536af6d7f41d0676a2b7005f5f0a0ca3b009a4b2
ikandaswamy <isha@couchbase.com>
2016-10-26 16:27:36 -0700

    MB-21479 Dont send request to cbq-engine when empty line in file
    Change-Id: Iff411c8459091599a97051f1d367c2be4ecd113b
    Reviewed-on: http://review.couchbase.org/69253
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Tested-by: Isha Kandaswamy <isha@couchbase.com>

--- shell/cbq/cmdexec.go
+++ shell/cbq/cmdexec.go
@@ -352,7 +352,7 @@ func readAndExec(liner *liner.State) (int, string) {
 			// Reached end of file. We are done. So break out of the loop.
 			// Do not require the last line on the file to have a \n.
 
-			if path == "" && final_input == " " {
+			if path == "" && final_input == " " || path == "" && final_input == ";" {
 				break
 			} else {
 				//This means we have some text on the last line
@@ -392,7 +392,11 @@ func readAndExec(liner *liner.State) (int, string) {
 		// Populate the final string to execute
 		final_input = strings.TrimSpace(final_input)
 
-		// Print the query along with printing the
+		if final_input == ";" {
+			continue
+		}
+
+		// Print the query along with printing the results
 		io.WriteString(command.W, final_input+"\n")
 
 		//Remove the ; before sending the query to execute
