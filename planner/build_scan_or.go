44907ea5f39da77225caf38847b8e2977c82f215
geraldss <gerald@couchbase.com>
2017-01-02 15:39:49 -0800

    MB-22148. Variable-length sarging for OR predicates.
    Change-Id: I0e568aa02b8c880a0fce37000c9bc30c50626584
    Reviewed-on: http://review.couchbase.org/71473
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Tested-by: Gerald Sangudi <gerald@couchbase.com>

--- planner/build_scan_or.go
+++ planner/build_scan_or.go
@@ -21,9 +21,105 @@ func (this *builder) buildOrScan(keyspace datastore.Keyspace, node *algebra.Keys
 	indexes []datastore.Index, primaryKey expression.Expressions, formalizer *expression.Formalizer) (
 	plan.Operator, int, error) {
 
+	tryPushdowns := this.cover != nil || this.order != nil || this.limit != nil ||
+		this.offset != nil || this.countAgg != nil || this.minAgg != nil
+
+	if tryPushdowns {
+		return this.buildOrScanTryPushdowns(keyspace, node, id, pred, limit, indexes, primaryKey, formalizer)
+	} else {
+		return this.buildOrScanNoPushdowns(keyspace, node, id, pred, limit, indexes, primaryKey, formalizer)
+	}
+}
+
+func (this *builder) buildOrScanTryPushdowns(keyspace datastore.Keyspace, node *algebra.KeyspaceTerm,
+	id expression.Expression, pred *expression.Or, limit expression.Expression,
+	indexes []datastore.Index, primaryKey expression.Expressions, formalizer *expression.Formalizer) (
+	plan.Operator, int, error) {
+
 	where := this.where
 	defer func() {
 		this.where = where
+	}()
+
+	coveringScans := this.coveringScans
+
+	var buf [16]plan.Operator
+	var scans []plan.Operator
+	if len(pred.Operands()) <= len(buf) {
+		scans = buf[0:0]
+	} else {
+		scans = make([]plan.Operator, 0, len(pred.Operands()))
+	}
+
+	var index datastore.Index
+	minSargLength := 0
+	distinct := false
+
+	for _, op := range pred.Operands() {
+		this.where = op
+		scan, termSargLength, err := this.buildTermScan(keyspace, node, id, op, limit, indexes, primaryKey, formalizer)
+		if scan == nil || err != nil {
+			this.coveringScans = coveringScans
+			return nil, 0, err
+		}
+
+		if distinctScan, ok := scan.(*plan.DistinctScan); ok {
+			scan = distinctScan.Scan()
+			distinct = true
+		}
+
+		if indexScan, ok := scan.(*plan.IndexScan); ok {
+			if index == nil {
+				index = indexScan.Index()
+			}
+
+			if index == indexScan.Index() {
+				scans = append(scans, scan)
+
+				if minSargLength == 0 || minSargLength > termSargLength {
+					minSargLength = termSargLength
+				}
+
+				continue
+			}
+		}
+
+		// TODO: Some work is duplicated here if no scan is performing pushdowns
+		this.coveringScans = coveringScans
+		return this.buildOrScanNoPushdowns(keyspace, node, id, pred, limit, indexes, primaryKey, formalizer)
+	}
+
+	spans := make(plan.Spans, 0, 2*len(scans))
+	for _, scan := range scans {
+		indexScan := scan.(*plan.IndexScan)
+		spans = append(spans, indexScan.Spans()...)
+	}
+
+	spans = deDupDiscardEmptySpans(spans)
+	indexScan0 := scans[0].(*plan.IndexScan)
+	indexScan0.SetSpans(spans)
+
+	if len(this.coveringScans) > len(coveringScans) {
+		this.coveringScans = append(coveringScans, indexScan0)
+	}
+
+	if distinct || len(spans) > 1 {
+		return plan.NewDistinctScan(indexScan0), minSargLength, nil
+	} else {
+		return indexScan0, minSargLength, nil
+	}
+}
+
+func (this *builder) buildOrScanNoPushdowns(keyspace datastore.Keyspace, node *algebra.KeyspaceTerm,
+	id expression.Expression, pred *expression.Or, limit expression.Expression,
+	indexes []datastore.Index, primaryKey expression.Expressions, formalizer *expression.Formalizer) (
+	plan.Operator, int, error) {
+
+	where := this.where
+	cover := this.cover
+	defer func() {
+		this.where = where
+		this.cover = cover
 	}()
 
 	this.cover = nil
@@ -36,8 +132,15 @@ func (this *builder) buildOrScan(keyspace datastore.Keyspace, node *algebra.Keys
 		this.order = nil
 	}
 
+	var buf [16]plan.Operator
+	var scans []plan.Operator
+	if len(pred.Operands()) <= len(buf) {
+		scans = buf[0:0]
+	} else {
+		scans = make([]plan.Operator, 0, len(pred.Operands()))
+	}
+
 	minSargLength := 0
-	scans := make([]plan.Operator, 0, len(pred.Operands()))
 
 	for _, op := range pred.Operands() {
 		this.where = op
