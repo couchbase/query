0886650b1349848fb7d087092ee23d0de830c91e
Johan Larson <johan.g.larson@gmail.com>
2017-03-16 07:52:35 -0700

    MB-23037 refactor store.Authorize() method for easier unit testing
    Change-Id: I05b2c712ddea141cd39d39b4fff3235effa5821a
    Reviewed-on: http://review.couchbase.org/75264
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Reviewed-by: Gerald Sangudi <gerald@couchbase.com>
    Tested-by: Johan Larson <johan.larson@couchbase.com>

---
+++ datastore/couchbase/auth_test.go
@@ -0,0 +1,114 @@
+//  Copyright (c) 2017 Couchbase, Inc.
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software distributed under the
+//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+//  either express or implied. See the License for the specific language governing permissions
+//  and limitations under the License.
+
+package couchbase
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/couchbase/cbauth"
+	"github.com/couchbase/query/datastore"
+)
+
+type authSourceImpl struct {
+	users []authUser
+}
+
+func (asi *authSourceImpl) adminIsOpen() bool {
+	return false
+}
+
+func (asi *authSourceImpl) auth(id, pwd string) (cbauth.Creds, error) {
+	for _, user := range asi.users {
+		if user.id == id {
+			if user.password == pwd {
+				return user, nil
+			}
+			return nil, fmt.Errorf("Invalid password %s supplied for user %s.", pwd, id)
+		}
+	}
+	return nil, fmt.Errorf("Could not find user %s.", id)
+}
+
+func (asi *authSourceImpl) isAuthTokenPresent(req *http.Request) bool {
+	return false
+}
+
+func (asi *authSourceImpl) authWebCreds(req *http.Request) (cbauth.Creds, error) {
+	return nil, fmt.Errorf("authWebCreds is not implemented")
+}
+
+// authUser implements cbauth.Creds
+type authUser struct {
+	id          string
+	password    string
+	permissions map[string]bool
+}
+
+func (a authUser) Name() string {
+	return a.id
+}
+
+func (a authUser) Source() string {
+	return "internal"
+}
+
+func (a authUser) IsAllowed(permission string) (bool, error) {
+	return a.permissions[permission], nil
+}
+
+type testCase struct {
+	purpose       string
+	creds         datastore.Credentials
+	shouldSucceed bool
+}
+
+func TestSimpleSelect(t *testing.T) {
+	privs := datastore.NewPrivileges()
+	privs.Add("testbucket", datastore.PRIV_READ)
+	privs.Add("testbucket", datastore.PRIV_QUERY_SELECT)
+
+	as := &authSourceImpl{
+		users: []authUser{
+			authUser{id: "bob", password: "pwbob",
+				permissions: map[string]bool{
+					"cluster.bucket[testbucket].data.docs!read":      true,
+					"cluster.bucket[testbucket].n1ql.select!execute": true,
+				},
+			},
+			authUser{id: "nancy", password: "pwnancy",
+				permissions: map[string]bool{
+					"cluster.bucket[testbucket].data.docs!read": true,
+				},
+			},
+		},
+	}
+
+	cases := []testCase{
+		testCase{purpose: "No Credentials", creds: datastore.Credentials{}, shouldSucceed: false},
+		testCase{purpose: "Insufficient Credentials", creds: datastore.Credentials{"nancy": "pwnancy"}, shouldSucceed: false},
+		testCase{purpose: "Wrong password", creds: datastore.Credentials{"bob": "badpassword"}, shouldSucceed: false},
+		testCase{purpose: "Works", creds: datastore.Credentials{"bob": "pwbob"}, shouldSucceed: true},
+	}
+
+	for _, c := range cases {
+		_, err := cbAuthorize(as, privs, c.creds, nil)
+		if c.shouldSucceed {
+			if err != nil {
+				t.Fatalf("Case %s should succeed, but it failed with error %v.", c.purpose, err)
+			}
+		} else {
+			if err == nil {
+				t.Fatalf("Case %s should fail, but it passed.", c.purpose)
+			}
+		}
+	}
+}
--- datastore/couchbase/couchbase.go
+++ datastore/couchbase/couchbase.go
@@ -252,53 +252,6 @@ func (s *store) NamespaceByName(name string) (p datastore.Namespace, e errors.Er
 	return p, nil
 }
 
-func doAuthByCreds(creds cbauth.Creds, bucket string, requested datastore.Privilege) (bool, error) {
-	var permission string
-	switch requested {
-	case datastore.PRIV_WRITE:
-		permission = fmt.Sprintf("cluster.bucket[%s].data.docs!write", bucket)
-	case datastore.PRIV_READ:
-		permission = fmt.Sprintf("cluster.bucket[%s].data.docs!read", bucket)
-	case datastore.PRIV_SYSTEM_READ:
-		permission = "cluster.n1ql.meta!read"
-	case datastore.PRIV_SECURITY_READ:
-		permission = "cluster.security!read"
-	case datastore.PRIV_SECURITY_WRITE:
-		permission = "cluster.security!write"
-	case datastore.PRIV_QUERY_SELECT:
-		permission = fmt.Sprintf("cluster.bucket[%s].n1ql.select!execute", bucket)
-	case datastore.PRIV_QUERY_UPDATE:
-		permission = fmt.Sprintf("cluster.bucket[%s].n1ql.update!execute", bucket)
-	case datastore.PRIV_QUERY_INSERT:
-		permission = fmt.Sprintf("cluster.bucket[%s].n1ql.insert!execute", bucket)
-	case datastore.PRIV_QUERY_DELETE:
-		permission = fmt.Sprintf("cluster.bucket[%s].n1ql.delete!execute", bucket)
-	case datastore.PRIV_QUERY_BUILD_INDEX:
-		permission = fmt.Sprintf("cluster.bucket[%s].n1ql.index!build", bucket)
-	case datastore.PRIV_QUERY_CREATE_INDEX:
-		permission = fmt.Sprintf("cluster.bucket[%s].n1ql.index!create", bucket)
-	case datastore.PRIV_QUERY_ALTER_INDEX:
-		permission = fmt.Sprintf("cluster.bucket[%s].n1ql.index!alter", bucket)
-	case datastore.PRIV_QUERY_DROP_INDEX:
-		permission = fmt.Sprintf("cluster.bucket[%s].n1ql.index!drop", bucket)
-	case datastore.PRIV_QUERY_LIST_INDEX:
-		permission = fmt.Sprintf("cluster.bucket[%s].n1ql.index!list", bucket)
-	default:
-		return false, fmt.Errorf("Invalid Privileges")
-	}
-	//logging.Errorf("JOHAN doAuthByCreds using %s seeking %s", creds.Name(), permission)
-
-	authResult, err := creds.IsAllowed(permission)
-	if err != nil || authResult == false {
-		//logging.Errorf("JOHAN permission denied")
-		return false, err
-	}
-
-	//logging.Errorf("JOHAN permission granted")
-	return true, nil
-
-}
-
 // The ns_server admin API is open iff we can access the /pools API without a password.
 func (s *store) adminIsOpen() bool {
 	url := s.connectionUrl + "/pools"
@@ -313,8 +266,19 @@ func (s *store) adminIsOpen() bool {
 	return true
 }
 
+func (s *store) auth(user, pwd string) (cbauth.Creds, error) {
+	return cbauth.Auth(user, pwd)
+}
+
+func (s *store) isAuthTokenPresent(req *http.Request) bool {
+	return cbauthi.IsAuthTokenPresent(req)
+}
+
+func (s *store) authWebCreds(req *http.Request) (cbauth.Creds, error) {
+	return cbauth.AuthWebCreds(req)
+}
+
 func (s *store) Authorize(privileges *datastore.Privileges, credentials datastore.Credentials, req *http.Request) (datastore.AuthenticatedUsers, errors.Error) {
-	//logging.Errorf("JOHAN: Authorize() with privileges %v credentials %v", privileges, credentials)
 	if s.CbAuthInit == false {
 		// cbauth is not initialized. Access to SASL protected buckets will be
 		// denied by the couchbase server
@@ -322,107 +286,7 @@ func (s *store) Authorize(privileges *datastore.Privileges, credentials datastor
 		return nil, nil
 	}
 
-	if credentials == nil {
-		credentials = make(datastore.Credentials)
-	}
-
-	authenticatedUsers := make(datastore.AuthenticatedUsers, 0, len(credentials))
-
-	// Build the credentials list.
-	credentialsList := make([]cbauth.Creds, 0, 2)
-	for username, password := range credentials {
-		var un string
-		userCreds := strings.Split(username, ":")
-		if len(userCreds) == 1 {
-			un = userCreds[0]
-		} else {
-			un = userCreds[1]
-		}
-
-		logging.Debugf(" Credentials for user %v", un)
-		creds, err := cbauth.Auth(un, password)
-		if err != nil {
-			logging.Debugf("Unable to authorize %s:%s.", username, password)
-			//logging.Errorf("JOHAN: could not authenticate %s:%s", username, password)
-		} else {
-			//logging.Errorf("JOHAN: authenticated %s:%s", username, password)
-			credentialsList = append(credentialsList, creds)
-			if un != "" {
-				authenticatedUsers = append(authenticatedUsers, un)
-			}
-		}
-	}
-
-	// Check for credentials from auth token in request
-	if req != nil && cbauthi.IsAuthTokenPresent(req) {
-		creds, err := cbauth.AuthWebCreds(req)
-		if err != nil {
-			logging.Debugf("Token auth error: %v", err)
-		} else {
-			credentialsList = append(credentialsList, creds)
-		}
-	}
-
-	// No privileges to check? Done.
-	if privileges == nil || len(privileges.List) == 0 {
-		return authenticatedUsers, nil
-	}
-
-	// No authenticated user, but credentials to check?
-	if len(credentialsList) == 0 {
-		if len(credentials) == 0 {
-			return nil, errors.NewDatastoreNoUserSupplied()
-		} else {
-			return nil, errors.NewDatastoreInvalidUsernamePassword()
-		}
-	}
-
-	// Check every requested privilege against the credentials list.
-	// if the authentication fails for any of the requested privileges return an error
-	for _, pair := range privileges.List {
-		keyspace := pair.Target
-		privilege := pair.Priv
-		if strings.Contains(keyspace, ":") {
-			q := strings.Split(keyspace, ":")
-			keyspace = q[1]
-		}
-
-		logging.Debugf("Authenticating for keyspace %s", keyspace)
-
-		thisBucketAuthorized := false
-		var rememberedError error
-
-		if keyspace == "nodes" && privilege == datastore.PRIV_SYSTEM_READ && s.adminIsOpen() {
-			// The system:nodes table follows the underlying ns_server API.
-			// If all tables have passwords, the API requires credentials.
-			// But if any don't, the API is open to read.
-			continue
-		}
-
-		// Check requested privilege against the list of credentials.
-		for _, creds := range credentialsList {
-			authResult, err := doAuthByCreds(creds, keyspace, privilege)
-
-			// Auth succeeded
-			if authResult == true {
-				thisBucketAuthorized = true
-				break
-			} else if err != nil {
-				rememberedError = err
-			}
-		}
-
-		if !thisBucketAuthorized {
-			msg := ""
-			if keyspace != "" {
-				msg = fmt.Sprintf(" Keyspace %s.", keyspace)
-			}
-			return nil, errors.NewDatastoreAuthorizationError(rememberedError, msg)
-		}
-	}
-
-	// If we got this far, every bucket is authorized. Success!
-	return authenticatedUsers, nil
+	return cbAuthorize(s, privileges, credentials, req)
 }
 
 func (s *store) CredsString(req *http.Request) string {
