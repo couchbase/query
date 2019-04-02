907c2ee6b1d06ee9f037ca3422860a539ca56b3f
Chang Liu <chang.liu@couchbase.com>
2019-04-02 11:04:02 -0700

    MB-33358 add unit testcases
    Change-Id: Ia023e6fbb68244c395067ae99b3c018b22569824
    Reviewed-on: http://review.couchbase.org/107185
    Reviewed-by: Bingjie Miao <bingjie.miao@couchbase.com>
    Tested-by: Chang Liu <chang.liu@couchbase.com>

---
+++ test/gsi/test_cases/advise/advise.go
@@ -0,0 +1,19 @@
+package advise
+
+import (
+	"github.com/couchbase/query/errors"
+	"github.com/couchbase/query/test/gsi"
+	"testing"
+)
+
+func runStmt(mockServer *gsi.MockServer, q string) ([]interface{}, []errors.Error, errors.Error) {
+	return gsi.RunStmt(mockServer, q)
+}
+
+func runMatch(filename string, prepared, explain bool, qc *gsi.MockServer, t *testing.T) {
+	gsi.RunMatch(filename, prepared, explain, qc, t)
+}
+
+func start_cs() *gsi.MockServer {
+	return gsi.Start_cs(true)
+}
---
+++ test/gsi/test_cases/advise/advise_test.go
@@ -0,0 +1,30 @@
+package advise
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"testing"
+)
+
+func TestAdvise(t *testing.T) {
+	if strings.ToLower(os.Getenv("GSI_TEST")) != "true" {
+		return
+	}
+
+	qc := start_cs()
+
+	fmt.Println("\n\nInserting values into Bucket \n\n ")
+	runMatch("insert.json", false, false, qc, t)
+
+	runMatch("case_advise.json", false, false, qc, t)
+
+	runStmt(qc, "CREATE PRIMARY INDEX ON shellTest")
+	_, _, errcs := runStmt(qc, "delete from shellTest where test_id IN [\"advise\"]")
+	if errcs != nil {
+		t.Errorf("did not expect err %s", errcs.Error())
+	}
+
+	runStmt(qc, "DROP PRIMARY INDEX ON shellTest")
+}
