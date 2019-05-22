67a1eb6bdc2385f9eeab4c595688713a97a63899
ikandaswamy <isha@couchbase.com>
2019-05-22 12:11:29 -0700

    Remove echo statements to allow go2xunit to work in jenkins runs
    Change-Id: I6af9501f2571d875648c2de73afe0007a8e15d7a
    Reviewed-on: http://review.couchbase.org/109535
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Tested-by: Isha Kandaswamy <isha@couchbase.com>

--- test/bucket_create.sh
+++ test/bucket_create.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo Creating Buckets
-
 Site=http://127.0.0.1:8091/pools/default/buckets
 Auth=Administrator:password
 bucket=(customer orders product purchase review shellTest)
@@ -9,39 +7,24 @@ q=${1:-250}
 
 for i in "${bucket[@]}"
 do
-echo curl -X POST -u $Auth -d name=$i -d ramQuotaMB=$q -d authType=sasl -d saslPassword=donotuse -d bucketType=couchbase $Site
-curl -X POST -u $Auth -d name=$i -d ramQuotaMB=$q -d authType=sasl -d saslPassword=donotuse -d bucketType=couchbase $Site
+  curl --silent -X POST -u $Auth -d name=$i -d ramQuotaMB=$q -d authType=sasl -d saslPassword=donotuse -d bucketType=couchbase $Site > /dev/null
 done
 
 cd filestore 
 
-echo mkdir -p data/dimestore/product
 mkdir -p data/dimestore/product
-
-echo mkdir data/dimestore/customer
 mkdir data/dimestore/customer
-
-echo mkdir data/dimestore/orders
 mkdir data/dimestore/orders
-
-echo mkdir data/dimestore/review
 mkdir data/dimestore/review
-
-echo mkdir data/dimestore/purchase
 mkdir data/dimestore/purchase
-
 cd ../
-
-echo Creating Users
 
 UsersSite=http://localhost:8091/settings/rbac/users/local/
 for i in "${bucket[@]}"
 do
-
-Id=${i}owner
-Name=OwnerOf${i}
-Password=${i}pass
-
-echo curl -X PUT $UsersSite$Id -d name=$Name -d roles=bucket_full_access[${i}] -d password=$Password -u $Auth
-curl -X PUT $UsersSite$Id -d name=$Name -d roles=bucket_full_access[${i}] -d password=$Password -u $Auth
+  Id=${i}owner
+  Name=OwnerOf${i}
+  Password=${i}pass
+  curl --silent -X PUT $UsersSite$Id -d name=$Name -d roles=bucket_full_access[${i}] -d password=$Password -u $Auth > /dev/null
 done
+
--- test/bucket_delete.sh
+++ test/bucket_delete.sh
@@ -1,35 +1,22 @@
 #!/bin/bash
 
-echo Delete Buckets
-
 Site=http://127.0.0.1:8091/pools/default/buckets/
 Auth=Administrator:password
 bucket=(customer orders product purchase review shellTest)
 
-
-echo POST /pools/default/buckets
-
 for i in "${bucket[@]}"
 do
-echo curl -u $Auth $Site$i
-curl -X DELETE -u $Auth $Site$i
+curl --silent -X DELETE -u $Auth $Site$i > /dev/null
 done
 
 cd filestore
-
-echo rm -rf data/
 rm -rf data/
-
 cd ../
-
-echo Delete Users
-
 
 UserSite=http://localhost:8091/settings/rbac/users/local/
 for i in "${bucket[@]}"
 do
 Id=${i}owner
-echo curl -X DELETE -u $Auth $UserSite$Id
-curl -X DELETE -u $Auth $UserSite$Id
+curl --silent -X DELETE -u $Auth $UserSite$Id > /dev/null 
 done
 
