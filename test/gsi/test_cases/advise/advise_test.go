ebab9f168e53982cd725b0d9897a6df6184c752d
Chang Liu <chang.liu@couchbase.com>
2019-07-15 12:14:57 -0700

    MB-35055 add support to advise self index(add testcases)
    Change-Id: Ia569b8d9919b7ecb848118f78d730786ba67fd29
    Reviewed-on: http://review.couchbase.org/112002
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Reviewed-by: Bingjie Miao <bingjie.miao@couchbase.com>
    Tested-by: Chang Liu <chang.liu@couchbase.com>

--- test/gsi/test_cases/advise/advise_test.go
+++ test/gsi/test_cases/advise/advise_test.go
@@ -19,6 +19,7 @@ func TestAdvise(t *testing.T) {
 	runMatch("insert.json", false, false, qc, t)
 
 	runMatch("case_advise.json", false, false, qc, t)
+	runMatch("case_advise_edgecase.json", false, false, qc, t)
 
 	runStmt(qc, "CREATE PRIMARY INDEX ON shellTest")
 	_, _, errcs := runStmt(qc, "delete from shellTest where test_id IN [\"advise\"]")
