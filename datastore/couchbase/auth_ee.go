addbc8dae677394e9136f904840d22f99fa10f0c
Johan Larson <johan.g.larson@gmail.com>
2019-08-07 07:12:28 -0700

    MB-29768 provide different advice about roles for EE and CE
    When a query fails because of insufficient permissions, we provide
    advice about what additional role is needed. The available roles
    are different in EE and CE. We therefore need to provide different
    advice. The advice we were providing was correct for EE only.
    
    Change-Id: Ia36a92bd4a004e56305b573aa7a8a8b454b7752b
    Reviewed-on: http://review.couchbase.org/113034
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Tested-by: Johan Larson <johan.larson@couchbase.com>

---
+++ datastore/couchbase/auth_ee.go
@@ -0,0 +1,66 @@
+//  Copyright (c) 2017 Couchbase, Inc.
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software distributed under the
+//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+//  either express or implied. See the License for the specific language governing permissions
+//  and limitations under the License.
+
+// +build enterprise
+
+package couchbase
+
+import (
+	"fmt"
+
+	"github.com/couchbase/query/auth"
+)
+
+func messageForDeniedPrivilege(pair auth.PrivilegePair) string {
+	_, keyspace := namespaceKeyspaceFromPrivPair(pair)
+
+	privilege := ""
+	role := ""
+	switch pair.Priv {
+	case auth.PRIV_READ:
+		privilege = "data read queries"
+		role = fmt.Sprintf("data_reader on bucket %s", keyspace)
+	case auth.PRIV_WRITE:
+		privilege = "data write queries"
+		role = fmt.Sprintf("data_reader_writer on bucket %s", keyspace)
+	case auth.PRIV_SYSTEM_READ:
+		privilege = "queries accessing the system tables"
+		role = "query_system_catalog"
+	case auth.PRIV_SECURITY_WRITE:
+		privilege = "queries updating user information"
+		role = "admin"
+	case auth.PRIV_SECURITY_READ:
+		privilege = "queries accessing user information"
+		role = "admin"
+	case auth.PRIV_QUERY_SELECT:
+		privilege = fmt.Sprintf("SELECT queries on the %s bucket", keyspace)
+		role = fmt.Sprintf("query_select on %s", keyspace)
+	case auth.PRIV_QUERY_UPDATE:
+		privilege = fmt.Sprintf("UPDATE queries on the %s bucket", keyspace)
+		role = fmt.Sprintf("query_update on %s", keyspace)
+	case auth.PRIV_QUERY_INSERT:
+		privilege = fmt.Sprintf("INSERT queries on the %s bucket", keyspace)
+		role = fmt.Sprintf("query_insert on %s", keyspace)
+	case auth.PRIV_QUERY_DELETE:
+		privilege = fmt.Sprintf("DELETE queries on the %s bucket", keyspace)
+		role = fmt.Sprintf("query_delete on %s", keyspace)
+	case auth.PRIV_QUERY_BUILD_INDEX, auth.PRIV_QUERY_CREATE_INDEX,
+		auth.PRIV_QUERY_ALTER_INDEX, auth.PRIV_QUERY_DROP_INDEX, auth.PRIV_QUERY_LIST_INDEX:
+		privilege = "index operations"
+		role = fmt.Sprintf("query_manage_index on %s", keyspace)
+	case auth.PRIV_QUERY_EXTERNAL_ACCESS:
+		privilege = "queries using the CURL() function"
+		role = "query_external_access"
+	default:
+		privilege = "this type of query"
+		role = "admin"
+	}
+
+	return fmt.Sprintf("User does not have credentials to run %s. Add role %s to allow the query to run.", privilege, role)
+}
--- datastore/couchbase/auth_test.go
+++ datastore/couchbase/auth_test.go
@@ -208,11 +208,11 @@ type deniedCase struct {
 func TestMessageForDeniedPrivilege(t *testing.T) {
 	cases := []deniedCase{
 		deniedCase{data: auth.PrivilegePair{Target: "testbucket", Priv: auth.PRIV_QUERY_SELECT},
-			expected: "User does not have credentials to run SELECT queries on the testbucket bucket. Add role query_select on testbucket to allow the query to run."},
+			expected: "User does not have credentials to run SELECT queries on the testbucket bucket. Add role bucket_full_access on testbucket to allow the query to run."},
 		deniedCase{data: auth.PrivilegePair{Target: ":testbucket", Priv: auth.PRIV_QUERY_DROP_INDEX},
-			expected: "User does not have credentials to run index operations. Add role query_manage_index on testbucket to allow the query to run."},
+			expected: "User does not have credentials to run index operations. Add role bucket_full_access on testbucket to allow the query to run."},
 		deniedCase{data: auth.PrivilegePair{Target: "", Priv: auth.PRIV_SYSTEM_READ},
-			expected: "User does not have credentials to run queries accessing the system tables. Add role query_system_catalog to allow the query to run."},
+			expected: "User does not have credentials to run queries accessing the system tables. Add role admin to allow the query to run."},
 	}
 
 	for i, c := range cases {
