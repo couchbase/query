01ccaa4589911755e95e3ef0270e2d52e6d965d0
Marco Greco <marco.greco@couchbase.com>
2020-01-07 18:14:26 +0000

    MB-37032 set hhtp read timeout
    Change-Id: Idd5d31cb91feb5eafc3be7b63f873ae8124203fa
    Reviewed-on: http://review.couchbase.org/120106
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Tested-by: Marco Greco <marco.greco@couchbase.com>

--- server/http/service_endpoint.go
+++ server/http/service_endpoint.go
@@ -76,7 +76,12 @@ func (this *HttpEndpoint) Listen() error {
 	ln, err := net.Listen("tcp", this.httpAddr)
 	if err == nil {
 		this.listener = ln
-		go http.Serve(ln, this.mux)
+		srv := &http.Server{
+			Handler:           this.mux,
+			ReadHeaderTimeout: 5 * time.Second,
+			ReadTimeout:       30 * time.Second,
+		}
+		go srv.Serve(ln)
 		logging.Infop("HttpEndpoint: Listen", logging.Pair{"Address", ln.Addr()})
 	}
 	return err
@@ -122,7 +127,12 @@ func (this *HttpEndpoint) ListenTLS() error {
 
 		tls_ln := tls.NewListener(ln, cfg)
 		this.listenerTLS = tls_ln
-		go http.Serve(tls_ln, this.mux)
+		srv := &http.Server{
+			Handler:           this.mux,
+			ReadHeaderTimeout: 5 * time.Second,
+			ReadTimeout:       30 * time.Second,
+		}
+		go srv.Serve(tls_ln)
 		logging.Infop("HttpEndpoint: ListenTLS", logging.Pair{"Address", ln.Addr()})
 	}
 
