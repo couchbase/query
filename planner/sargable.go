490cb13077d8ea8295ca370752fa39427cefba2a
sitaramv <sitaram.vemulapalli@couchbase.com>
2020-03-05 16:08:53 -0800

    MB-31203 Index selection is based on continuous keys only (minor update)
    Change-Id: I87a9a4ffc750288bccc546047ce840d66ea13954
    Reviewed-on: http://review.couchbase.org/123347
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Bingjie Miao <bingjie.miao@couchbase.com>
    Tested-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>

--- planner/sargable.go
+++ planner/sargable.go
@@ -63,15 +63,15 @@ func sargableForOr(or *expression.Or, keys expression.Expressions, missing, gsi 
 
 	for _, c := range or.Operands() {
 		cmin, cmax, csum := SargableFor(c, keys, missing, gsi)
-		if cmin == 0 || cmax == 0 || csum < cmin || csum < cmax {
+		if cmin == 0 || cmax == 0 || csum < cmin {
 			return 0, 0, 0
 		}
 
-		if min == 0 || cmin < min {
+		if min == 0 || min > cmin {
 			min = cmin
 		}
 
-		if max == 0 || cmax < max {
+		if max == 0 || max < cmax {
 			max = cmax
 		}
 
