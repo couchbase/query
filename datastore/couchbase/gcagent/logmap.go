9e2c3faa75007fa8cda69f2e1f63b9a2b603b789
sitaramv <sitaram.vemulapalli@couchbase.com>
2020-09-28 07:17:16 -0700

    MB-38653. Map gocbcore log messages to query logging messages
    Change-Id: I3d58c9359980c6d48fa940cea563cb228cee16a3
    Reviewed-on: http://review.couchbase.org/c/query/+/137042
    Reviewed-by: Bingjie Miao <bingjie.miao@couchbase.com>
    Reviewed-by: Marco Greco <marco.greco@couchbase.com>
    Tested-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>

---
+++ datastore/couchbase/gcagent/logmap.go
@@ -0,0 +1,43 @@
+//  Copyright (c) 2020 Couchbase, Inc.
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software distributed under the
+//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+//  either express or implied. See the License for the specific language governing permissions
+//  and limitations under the License.
+
+package gcagent
+
+import (
+	"github.com/couchbase/gocbcore/v9"
+	"github.com/couchbase/query/logging"
+)
+
+type GocbcoreLogger struct {
+}
+
+var gocbcoreLogger GocbcoreLogger
+
+func (l GocbcoreLogger) Log(level gocbcore.LogLevel, offset int, format string,
+	args ...interface{}) error {
+	prefixedFormat := "(GOCBCORE) " + format
+	switch level {
+	case gocbcore.LogError:
+		logging.Errorf(prefixedFormat, args...)
+	case gocbcore.LogWarn:
+		logging.Warnf(prefixedFormat, args...)
+	case gocbcore.LogInfo:
+		logging.Infof(prefixedFormat, args...)
+	case gocbcore.LogDebug:
+		logging.Debugf(prefixedFormat, args...)
+	default:
+		logging.Tracef(prefixedFormat, args...)
+	}
+
+	return nil
+}
+
+func init() {
+	gocbcore.SetLogger(gocbcoreLogger)
+}
