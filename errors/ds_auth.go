3c6c7747c2b8f5d58a537460af072ba5eaae1a73
dhaggart <donald.haggart@couchbase.com>
2021-01-22 16:54:47 +0000

    MB-42807 Cleaned up errors/ds_view.go moving 3 remaining auth-related errors into existing ds_auth.go & removing the un-used functions
    Change-Id: I0c41dabdce9d97fd59fe1419f581f5c008256510
    Reviewed-on: http://review.couchbase.org/c/query/+/144164
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Reviewed-by: Marco Greco <marco.greco@couchbase.com>
    Tested-by: Donald Haggart <donald.haggart@couchbase.com>

--- errors/ds_auth.go
+++ errors/ds_auth.go
@@ -18,3 +18,20 @@ func NewDatastoreAuthorizationError(e error) Error {
 	return &err{level: EXCEPTION, ICode: DS_AUTH_ERROR, IKey: "datastore.couchbase.authorization_error", ICause: e,
 		InternalMsg: "Unable to authorize user.", InternalCaller: CallerN(1)}
 }
+
+// Error codes 13010-13011 are retired. Do not reuse.
+
+func NewDatastoreClusterError(e error, msg string) Error {
+	return &err{level: EXCEPTION, ICode: 13012, IKey: "datastore.couchbase.cluster_error", ICause: e,
+		InternalMsg: "Error retrieving cluster " + msg, InternalCaller: CallerN(1)}
+}
+
+func NewDatastoreUnableToRetrieveRoles(e error) Error {
+	return &err{level: EXCEPTION, ICode: 13013, IKey: "datastore.couchbase.retrieve_roles", ICause: e,
+		InternalMsg: "Unable to retrieve roles from server.", InternalCaller: CallerN(1)}
+}
+
+func NewDatastoreInsufficientCredentials(msg string) Error {
+	return &err{level: EXCEPTION, ICode: 13014, IKey: "datastore.couchbase.insufficient_credentials",
+		InternalMsg: msg, InternalCaller: CallerN(1)}
+}
