a7cf55ca44a0239f3302703d7c98c0f9c69a3063
miaobingjie <bingjie.miao@couchbase.com>
2021-02-02 10:10:24 -0700

    MB-43804 Proper handling of subset of LIKE expressions
    Change-Id: I7172f61499f708c8d9bf47be78f9d71193a64e03
    Reviewed-on: http://review.couchbase.org/c/query/+/144973
    Reviewed-by: Marco Greco <marco.greco@couchbase.com>
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Tested-by: Bingjie Miao <bingjie.miao@couchbase.com>

--- plannerbase/subset_like.go
+++ plannerbase/subset_like.go
@@ -11,6 +11,8 @@ package plannerbase
 
 import (
 	"math"
+	"regexp"
+	"strings"
 
 	"github.com/couchbase/query/expression"
 )
@@ -42,6 +44,19 @@ func (this *subset) visitLike(expr expression.LikeFunction) (interface{}, error)
 		return this.visitDefault(expr)
 	}
 
+	switch expr2 := expr2.(type) {
+	case *expression.Like:
+		if _, ok := expr.(*expression.Like); !ok {
+			return false, nil
+		}
+		return likeSubset(expr, expr2, re, prefix)
+	case *expression.RegexpLike:
+		if _, ok := expr.(*expression.RegexpLike); !ok {
+			return false, nil
+		}
+		return likeSubset(expr, expr2, re, prefix)
+	}
+
 	var and expression.Expression
 	le := expression.NewLE(expression.NewConstant(prefix), expr.First())
 	last := len(prefix) - 1
@@ -58,4 +73,37 @@ func (this *subset) visitLike(expr expression.LikeFunction) (interface{}, error)
 	}
 
 	return and.Accept(this)
+}
+
+func likeSubset(expr1, expr2 expression.LikeFunction, re1 *regexp.Regexp, prefix1 string) (interface{}, error) {
+	// make sure left-hand side matches
+	if expr1 == nil || expr2 == nil || re1 == nil || prefix1 == "" || !expr1.First().EquivalentTo(expr2.First()) {
+		return false, nil
+	}
+
+	re2 := expr2.Regexp()
+	if re2 == nil || re1.NumSubexp() != re2.NumSubexp() {
+		return false, nil
+	}
+
+	prefix2, complete := re2.LiteralPrefix()
+	if complete || prefix2 == "" {
+		return false, nil
+	}
+
+	// make sure prefix of re1 is superset of (i.e. more restrictive) prefix of re2
+	len1 := len(prefix1)
+	len2 := len(prefix2)
+	if len1 < len2 || prefix1[:len2] != prefix2 {
+		return false, nil
+	}
+
+	// make sure rest of pattern (excluding the prefix) is identical
+	sub1 := strings.Replace(re1.String(), prefix1, "", 1)
+	sub2 := strings.Replace(re2.String(), prefix2, "", 1)
+	if sub1 != sub2 {
+		return false, nil
+	}
+
+	return true, nil
 }
