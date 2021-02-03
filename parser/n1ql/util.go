fbdec57e455bcf73dcd8edcf4ef0938874aab122
dhaggart <donald.haggart@couchbase.com>
2021-02-03 17:20:07 +0000

    MB-30176 Handle UTF16 surrogates
    Change-Id: Iba42c173ffcb60ee6e13277751148e89316aab94
    Reviewed-on: http://review.couchbase.org/c/query/+/145124
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Reviewed-by: Marco Greco <marco.greco@couchbase.com>
    Tested-by: Donald Haggart <donald.haggart@couchbase.com>

--- parser/n1ql/util.go
+++ parser/n1ql/util.go
@@ -11,19 +11,49 @@ package n1ql
 
 import (
 	"errors"
+	"unicode"
+	"unicode/utf16"
 	"unicode/utf8"
 	"unsafe"
 )
 
-// Handle permitted JSON escape sequences along with appropriate quotation mark escaping
+// Extract first 4 characters in provided string as (ASCII) hex digits of a UTF16 encoding
+func getUTF16Rune(s string) (rune, error) {
+	var rn rune = rune(0x0)
+	for _, b := range []byte(s[0:4]) {
+		b -= '0'
+		if b > 9 {
+			b = b - 7
+			if b > 15 {
+				b = b - 32
+			}
+			if b < 10 {
+				return unicode.ReplacementChar, errors.New("invalid escape sequence")
+			}
+		}
+		if b < 0 || b > 15 {
+			return unicode.ReplacementChar, errors.New("invalid escape sequence")
+		}
+		rn = rn << 4
+		rn = rn | rune(b)
+	}
+	return rn, nil
+}
+
+// Handle permitted JSON escape sequences along with appropriate quotation mark escaping (allows '' as an escaped ')
 // Ref: https://www.json.org/json-en.html
 func ProcessEscapeSequences(s string) (t string, e error) {
 	b := make([]byte, len(s)-2)
 	w := 0
-	bq := false
-	for r := 1; r < len(s)-1; r++ {
+	for r := 1; r < len(s)-1; {
 		c := s[r]
-		if bq {
+		r++
+		if c == '\\' {
+			if r >= len(s)-1 {
+				return t, errors.New("invalid escape sequence")
+			}
+			c = s[r]
+			r++
 			switch c {
 			case 'b':
 				b[w] = '\b'
@@ -53,51 +83,42 @@ func ProcessEscapeSequences(s string) (t string, e error) {
 				b[w] = c
 				w++
 			case 'u':
-				// next 4 chars are hex digits which we build a UTF-8 encoded rune from
-				r++
 				if r+4 > len(s)-1 {
-					return t, errors.New("invalid unicode escape sequence")
+					return t, errors.New("invalid escape sequence")
 				}
-				var rn rune = rune(0x0)
-				for end := r + 4; r < end; r++ {
-					b := byte(s[r]) - '0'
-					if b > 9 {
-						b = b - 7
-						if b > 15 {
-							b = b - 32
+				rn, err := getUTF16Rune(s[r:])
+				if err != nil {
+					return t, err
+				}
+				r += 4
+				if utf16.IsSurrogate(rn) {
+					if r+6 > len(s)-1 || s[r] != '\\' || s[r+1] != 'u' {
+						rn = unicode.ReplacementChar
+					} else {
+						r += 2
+						rn2, err := getUTF16Rune(s[r:])
+						if err != nil {
+							return t, err
 						}
-						if b < 10 {
-							return t, errors.New("invalid unicode escape sequence")
-						}
+						r += 4
+						// returns unicode.ReplacementChar if not a valid surrogate which we'll use instead
+						rn = utf16.DecodeRune(rn, rn2)
 					}
-					if b < 0 || b > 15 {
-						return t, errors.New("invalid unicode escape sequence")
-					}
-					rn = rn << 4
-					rn = rn | rune(b)
 				}
-				r-- // loop will increment too
-				buf := make([]byte, utf8.UTFMax)
-				n := utf8.EncodeRune(buf, rn)
-				for i := 0; i < n; i++ {
-					b[w] = buf[i]
-					w++
-				}
+				w += utf8.EncodeRune(b[w:], rn)
 			default:
 				return t, errors.New("invalid escape sequence")
 			}
-			bq = false
-		} else if c == '\\' {
-			bq = true
 		} else {
+			// we don't need validate UTF8 sequences so we can simply copy them byte-by-byte
 			b[w] = c
 			w++
 			// if single quoted, allow '' as an escaped single quote
 			if s[0] == '\'' && c == '\'' {
-				r++
 				if r >= len(s)-1 || s[r] != '\'' {
 					return t, errors.New("unescaped embedded quote")
 				}
+				r++
 			} else if s[0] == c {
 				return t, errors.New("unescaped embedded quote")
 			}
