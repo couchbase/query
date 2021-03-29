50a494aa85efdb90575cb2ce8bba5c5b6d6fd77b
dhaggart <donald.haggart@couchbase.com>
2021-03-29 19:02:24 +0100

    MB-46889 Add licence notice generation to build script
    Change-Id: Ibd33ce31981b8639592936e6291db48f9c1f13d9
    Reviewed-on: http://review.couchbase.org/c/query/+/155727
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Tested-by: Donald Haggart <donald.haggart@couchbase.com>
    (cherry picked from commit 685f8a159f42cb3692b8d5eac1c873975fcdc99e)
    Reviewed-on: http://review.couchbase.org/c/query/+/157495
    Reviewed-by: Marco Greco <marco.greco@couchbase.com>

--- parser/n1ql/build.sh
+++ parser/n1ql/build.sh
@@ -19,8 +19,18 @@ then
     go build
     cd $BACK
     $GOPATH/src/$NEX/nex n1ql.nex
+    cat << EOF > n1ql.nn.tmp
+//  Copyright 2014-Present Couchbase, Inc.
+//
+//  Use of this software is governed by the Business Source License included in
+//  the file licenses/Couchbase-BSL.txt.  As of the Change Date specified in that
+//  file, in accordance with the Business Source License, use of this software will
+//  be governed by the Apache License, Version 2.0, included in the file
+//  licenses/APL.txt.
+
+EOF
     awk '/NEX_END_OF_LEXER_STRUCT/ { print "curOffset int\nreportError func(what string)" }
-	 { print }' n1ql.nn.go > n1ql.nn.tmp
+	 { print }' n1ql.nn.go >> n1ql.nn.tmp
     mv n1ql.nn.tmp n1ql.nn.go
     go fmt n1ql.nn.go
 fi
