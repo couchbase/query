98b6f8f8f4c0e8066edf591d9f05287875c28f18
miaobingjie <bingjie.miao@couchbase.com>
2021-07-02 11:56:27 -0600

    MB-47220 Allow index span to be exact if the same simple predicate generates an exact span ahead of a non-exact span
    Change-Id: I1fbbcd2ea77513e9c296556d8e9dae628624be18
    Reviewed-on: http://review.couchbase.org/c/query/+/156906
    Reviewed-by: Donald Haggart <donald.haggart@couchbase.com>
    Tested-by: Bingjie Miao <bingjie.miao@couchbase.com>

--- planner/sarg.go
+++ planner/sarg.go
@@ -230,6 +230,13 @@ func getSargSpans(pred expression.Expression, sargKeys expression.Expressions, i
 	baseKeyspace *base.BaseKeyspace, keyspaceNames map[string]string, advisorValidate bool,
 	context *PrepareContext) ([]SargSpans, bool, error) {
 
+	// is the predicate simple?
+	simple := true
+	switch pred.(type) {
+	case *expression.And, *expression.Or:
+		simple = false
+	}
+
 	n := len(sargKeys)
 
 	exactSpan := true
@@ -259,7 +266,15 @@ func getSargSpans(pred expression.Expression, sargKeys expression.Expressions, i
 				return []SargSpans{_EMPTY_SPANS}, true, nil
 			}
 
-			exactSpan = exactSpan && rs.Exact()
+			if simple && rs.Exact() {
+				// if the same simple predicate can be used to sarg multiple
+				// index keys, and generate an exact span for one of the index key,
+				// we can set exactSpan to true even if it is not exact for
+				// a different key (which appears after this index key)
+				exactSpan = true
+			} else {
+				exactSpan = exactSpan && rs.Exact()
+			}
 		} else if exactSpan && pred.DependsOn(sargKeys[i]) {
 			exactSpan = false
 		}
