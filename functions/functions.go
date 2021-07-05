efdadead0ed9112d8ebcdedd2906454f04c16de9
Marco Greco <marco.greco@couchbase.com>
2021-07-05 18:24:59 +0100

    MB-44263 filter and remap for UDF backup endpoints
    Change-Id: I75ec35d0dafc537e9974563deda72d5b0f892adf
    Reviewed-on: http://review.couchbase.org/c/query/+/156988
    Reviewed-by: Donald Haggart <donald.haggart@couchbase.com>
    Tested-by: Marco Greco <marco.greco@couchbase.com>

--- functions/functions.go
+++ functions/functions.go
@@ -43,6 +43,7 @@ const _LIMIT = 16384
 
 type FunctionName interface {
 	Path() []string
+	Remap(p []string)
 	Name() string
 	Key() string
 	IsGlobal() bool
@@ -174,6 +175,13 @@ func mockFunction(namespace string, name string) FunctionName {
 
 func (name *mockName) Path() []string {
 	return []string{name.namespace, name.name}
+}
+
+func (name *mockName) Remap(p []string) {
+	if len(p) == 2 {
+		name.namespace = p[0]
+		name.name = p[1]
+	}
 }
 
 func (name *mockName) Name() string {
