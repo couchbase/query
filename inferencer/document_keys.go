ab1a65b94c38468ee976b566a75091da56858aaf
miaobingjie <bingjie.miao@couchbase.com>
2021-11-12 14:19:12 -0700

    MB-49504 Gather average document key size in case no histograms are constructed
    For UPDATE STATISTICS with INDEX ALL option, the list of terms
    that require histograms is constructed by gathering index keys
    from all indexes defined. In case there is only a primary index
    defined and there are no terms that needs a histogram, no
    sampling is done. Is such cases, we do not have a document key
    size estimate since there were no sample documents.
    
    There is currently logic in the index advisor for gathering
    average document key size, for purpose of estimating index
    size. Move the logic to inferencer such that it can be shared
    between index advisor and update statistics.
    
    Change-Id: Ie5dd5cd5e1fd9365200fdad835f45440e425d580
    Reviewed-on: https://review.couchbase.org/c/query/+/165660
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Tested-by: Bingjie Miao <bingjie.miao@couchbase.com>

---
+++ inferencer/document_keys.go
@@ -0,0 +1,57 @@
+// Copyright 2021-Present Couchbase, Inc.
+//
+// Use of this software is governed by the Business Source License included in
+// the file licenses/Couchbase-BSL.txt.  As of the Change Date specified in that
+// file, in accordance with the Business Source License, use of this software will
+// be governed by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package inferencer
+
+import (
+	"math"
+	"time"
+
+	"github.com/couchbase/query/datastore"
+	"github.com/couchbase/query/errors"
+)
+
+func GetAvgDocKeyLen(retriever DocumentRetriever, conn *datastore.ValueConnection,
+	context datastore.QueryContext, timeout int32) (int64, errors.Error) {
+
+	start := time.Now()
+	count := 0
+	totalLength := 0
+
+	for {
+		if conn != nil {
+			select {
+			case <-conn.StopChannel():
+				return -1, nil
+			default:
+			}
+		}
+
+		key, doc, err := retriever.GetNextDoc(context)
+		if err != nil {
+			return -1, err
+		}
+
+		if doc == nil {
+			// no more docs
+			break
+		}
+		count += 1
+		totalLength += len(key)
+
+		if int32(time.Now().Sub(start)/time.Second) > timeout {
+			break
+		}
+	}
+
+	if count > 0 {
+		return int64(math.Round(float64(totalLength) / float64(count))), nil
+	}
+
+	return -1, nil
+}
