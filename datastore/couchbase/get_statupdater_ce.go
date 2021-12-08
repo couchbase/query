d40ec39858d9cdc9f71c1cd150505286c366eb16
Couchbase Build Team <build-team@couchbase.com>
2021-12-08 06:08:37 -0800

    MB-49140: Update BSL license version/change date for Neo
    Change-Id: I99abb0151fa8add2b364a2fab67f9aa066f6ca8c
    Reviewed-on: https://review.couchbase.org/c/query/+/167232
    Reviewed-by: Donald Haggart <donald.haggart@couchbase.com>
    Tested-by: Blair Watt <blair.watt@couchbase.com>

--- datastore/couchbase/get_statupdater_ce.go
+++ datastore/couchbase/get_statupdater_ce.go
@@ -1,10 +1,10 @@
 // Copyright 2018-Present Couchbase, Inc.
 //
-// Use of this software is governed by the Business Source License included
-// in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
-// in that file, in accordance with the Business Source License, use of this
-// software will be governed by the Apache License, Version 2.0, included in
-// the file licenses/APL2.txt.
+// Use of this software is governed by the Business Source License included in
+// the file licenses/BSL-Couchbase.txt.  As of the Change Date specified in that
+// file, in accordance with the Business Source License, use of this software
+// will be governed by the Apache License, Version 2.0, included in the file
+// licenses/APL2.txt.
 //
 // Currently, the community edition does not have access to update
 // statistics, so this stub returns an error.
--- datastore/couchbase/get_statupdater_ee.go
+++ datastore/couchbase/get_statupdater_ee.go
@@ -1,10 +1,10 @@
 // Copyright 2018-Present Couchbase, Inc.
 //
-// Use of this software is governed by the Business Source License included
-// in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
-// in that file, in accordance with the Business Source License, use of this
-// software will be governed by the Apache License, Version 2.0, included in
-// the file licenses/APL2.txt.
+// Use of this software is governed by the Business Source License included in
+// the file licenses/BSL-Couchbase.txt.  As of the Change Date specified in that
+// file, in accordance with the Business Source License, use of this software
+// will be governed by the Apache License, Version 2.0, included in the file
+// licenses/APL2.txt.
 //
 // The enterprise edition has access to couchbase/query-ee, which
 // includes update statistics. This file is only built in with
