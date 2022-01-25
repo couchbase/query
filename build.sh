4bddc327d9f82f4c75009897919094f3492e4848
sitaramv <sitaram.vemulapalli@couchbase.com>
2022-01-25 06:44:03 -0800

    use gocbcore tag 10.0.7
    Change-Id: I6d934fc41b599b57b77be5630f2a42c52bb6c624
    Reviewed-on: https://review.couchbase.org/c/query/+/169429
    Reviewed-by: Donald Haggart <donald.haggart@couchbase.com>
    Reviewed-by: Abhinav Dangeti <abhinav@couchbase.com>
    Tested-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>

--- build.sh
+++ build.sh
@@ -75,18 +75,18 @@ DevStandaloneSetup() {
        then
            (cd ..; rm -rf gocbcore/v9; git clone -b v9.1.8 https://github.com/couchbase/gocbcore.git gocbcore/v9)
        fi
-    # gocbcore points to master; gocbcore/v10 points to 10.0.6
+    # gocbcore points to master; gocbcore/v10 points to 10.0.7
        if [[ -d ../gocbcore/v10 ]]
        then
            cd ../gocbcore/v10
-           C=`git describe --tags|grep -c "v10.0.6"`
+           C=`git describe --tags|grep -c "v10.0.7"`
            cd -
        else
            C=0
        fi
        if [[ $C -eq 0 ]]
        then
-           (cd ..; rm -rf gocbcore/v10; git clone -b v10.0.6 https://github.com/couchbase/gocbcore.git gocbcore/v10)
+           (cd ..; rm -rf gocbcore/v10; git clone -b v10.0.7 https://github.com/couchbase/gocbcore.git gocbcore/v10)
        fi
     # bleve version
        if [[ ! -d ../../blevesearch/bleve/v2 ]]; then
