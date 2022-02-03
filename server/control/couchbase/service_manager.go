d67ffbbd06eba578f71a9437e896dd480e5da307
dhaggart <donald.haggart@couchbase.com>
2022-02-03 14:35:25 +0000

    MB-50716 Reuse prepared admin operations where possible.
    When processing a PreppareTopologyChange callback, we previously
    regenerated the prepared admin operations for all nodes in the KeepNodes
    list before assigning this as the active servers list.  This change aims
    to reduce the processing time in PrepareTopologyChange by reusing any
    prepared operations for nodes in the existing servers list.  In the
    majority of cases reuse makes good sense but it does mean that, should it
    be possible for the underying query node information to change, the
    prepared operation will be stale and never be replaced with an updated
    version.
    
    This also caches the host name so as to avoid repeated UUIDToHost
    calls, again primarily to save processing time in PrepareTopologyChange
    invocations.
    
    Correct parallel node ejection issue.
    
    Change-Id: I334c0be78cb42f29554ecb533a021cb649806cb7
    Reviewed-on: https://review.couchbase.org/c/query/+/170091
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Reviewed-by: Marco Greco <marco.greco@couchbase.com>
    Tested-by: Donald Haggart <donald.haggart@couchbase.com>

--- server/control/couchbase/service_manager.go
+++ server/control/couchbase/service_manager.go
@@ -34,11 +34,17 @@ type ServiceMgr struct {
 	waiters  waiters
 }
 
+// to reduce the occasions when host is looked up from node ID, cache it here
+type queryServer struct {
+	host     string
+	nodeInfo service.NodeInfo
+}
+
 type state struct {
 	rev      uint64
 	changeID string
-	servers  []service.NodeInfo
-	eject    []service.NodeInfo
+	servers  []queryServer
+	eject    []queryServer
 }
 
 type waiter chan state
@@ -91,11 +97,11 @@ func (m *ServiceMgr) setInitialNodeList() {
 	topology := distributed.RemoteAccess().GetNodeNames()
 
 	info := make([]rune, 0, len(topology)*32)
-	nodeList := make([]service.NodeInfo, 0)
+	nodeList := make([]queryServer, 0)
 	for _, nn := range topology {
 		ps := prepareOperation(nn, "ServiceMgr::setInitialNodeList")
 		uuid := distributed.RemoteAccess().NodeUUID(nn)
-		nodeList = append(nodeList, service.NodeInfo{service.NodeID(uuid), service.Priority(0), ps})
+		nodeList = append(nodeList, queryServer{nn, service.NodeInfo{service.NodeID(uuid), service.Priority(0), ps}})
 		info = append(info, []rune(uuid)...)
 		info = append(info, '[')
 		info = append(info, []rune(nn)...)
@@ -157,13 +163,14 @@ func (m *ServiceMgr) GetTaskList(rev service.Revision, cancel service.Cancel) (*
 		running := 0
 		if eject != nil {
 			for _, e := range eject {
-				if e.Opaque == nil {
-					e.Priority = -1
+				if e.nodeInfo.Opaque == nil {
+					e.nodeInfo.Priority = -1
 				}
-				if e.Priority < 0 {
+				if e.nodeInfo.Priority < 0 {
 					continue
 				}
-				res, err := distributed.RemoteAccess().ExecutePreparedAdminOp(e.Opaque, "GET", "", nil, distributed.NO_CREDS, "")
+				res, err := distributed.RemoteAccess().ExecutePreparedAdminOp(e.nodeInfo.Opaque, "GET", "", nil,
+					distributed.NO_CREDS, "")
 				if res != nil && err == nil {
 					var status struct {
 						Code int32 `json:"code"`
@@ -172,10 +179,10 @@ func (m *ServiceMgr) GetTaskList(rev service.Revision, cancel service.Cancel) (*
 					if jerr == nil && errors.ErrorCode(status.Code) != errors.E_SERVICE_SHUT_DOWN {
 						running++
 					} else {
-						e.Priority = -1
+						e.nodeInfo.Priority = -1
 					}
 				} else {
-					e.Priority = -1
+					e.nodeInfo.Priority = -1
 				}
 			}
 		}
@@ -211,7 +218,6 @@ func (m *ServiceMgr) GetTaskList(rev service.Revision, cancel service.Cancel) (*
 
 func (m *ServiceMgr) CancelTask(id string, rev service.Revision) error {
 	logging.Debuga(func() string { return fmt.Sprintf("ServiceMgr::CancelTask entry %v %v", id, DecodeRev(rev)) })
-	defer logging.Debugf("ServiceMgr::CancelTask exit")
 
 	timeout := time.Duration(0)
 	data := "cancel=true"
@@ -221,7 +227,7 @@ func (m *ServiceMgr) CancelTask(id string, rev service.Revision) error {
 	currentTask := fmt.Sprintf("shutdown/monitor/%s", m.changeID)
 	if currentTask == id {
 		if m.eject != nil {
-			servers := make([]service.NodeInfo, 0, len(m.eject))
+			servers := make([]queryServer, 0, len(m.eject))
 			servers = append(servers, m.servers...)
 			timedOut := false
 			info := make([]rune, 0, len(m.eject)*33)
@@ -230,27 +236,27 @@ func (m *ServiceMgr) CancelTask(id string, rev service.Revision) error {
 			// in parallel in case some take time to reach
 			for _, e := range m.eject {
 				go func() {
-					if e.Opaque == nil {
+					if e.nodeInfo.Opaque == nil {
 						// if we failed to prepare it before now, we could well be too late but try again anyway
-						e.Opaque = prepareOperation(string(e.NodeID), "ServiceMgr::CancelTask")
+						e.nodeInfo.Opaque = prepareOperation(e.host, "ServiceMgr::CancelTask")
 					}
-					_, err := distributed.RemoteAccess().ExecutePreparedAdminOp(e.Opaque, "POST", data, nil,
+					_, err := distributed.RemoteAccess().ExecutePreparedAdminOp(e.nodeInfo.Opaque, "POST", data, nil,
 						distributed.NO_CREDS, "")
 					if err == nil {
 						mutex.Lock()
 						if !timedOut {
-							e.Priority = 0
+							e.nodeInfo.Priority = 0
 							servers = appendInOrder(servers, e)
-							info = append(info, []rune(e.NodeID)...)
+							info = append(info, []rune(e.nodeInfo.NodeID)...)
 							info = append(info, ' ')
 						}
 						mutex.Unlock()
 						logging.Debuga(func() string {
-							return fmt.Sprintf("ServiceMgr::CancelTask cancelled shutdown down on '%s'", string(e.NodeID))
+							return fmt.Sprintf("ServiceMgr::CancelTask cancelled shutdown down on '%s'", string(e.nodeInfo.NodeID))
 						})
 					} else {
-						logging.Infof("ServiceMgr::CancelTask failed to cancel shutdown on '%s' (op:%v): %v", string(e.NodeID),
-							e.Opaque, err)
+						logging.Infof("ServiceMgr::CancelTask failed to cancel shutdown on '%s' (op:%v): %v",
+							string(e.nodeInfo.NodeID), e.nodeInfo.Opaque, err)
 					}
 					done.Incr()
 				}()
@@ -278,28 +284,29 @@ func (m *ServiceMgr) CancelTask(id string, rev service.Revision) error {
 				info = info[:0]
 			}
 			for _, n := range m.servers {
-				info = append(info, []rune(n.NodeID)...)
+				info = append(info, []rune(n.nodeInfo.NodeID)...)
 				info = append(info, '[')
-				info = append(info, []rune(distributed.RemoteAccess().UUIDToHost(string(n.NodeID)))...)
+				info = append(info, []rune(n.host)...)
 				info = append(info, ']')
 				info = append(info, ' ')
 			}
 			logging.Infof("Current topology: %s", string(info))
+			logging.Debugf("ServiceMgr::CancelTask exit")
 			return nil
 		} else {
-			logging.Debugf("ServiceMgr::CancelTask ejected nodes list is empty.")
+			logging.Debugf("ServiceMgr::CancelTask exit: ejected nodes list is empty.")
 			return service.ErrConflict
 		}
 	}
-	logging.Debuga(func() string { return fmt.Sprintf("ServiceMgr::CancelTask unknown task (%v).", id) })
+	logging.Debuga(func() string { return fmt.Sprintf("ServiceMgr::CancelTask exit: unknown task (%v).", id) })
 	return service.ErrNotFound
 }
 
-func appendInOrder(list []service.NodeInfo, item service.NodeInfo) []service.NodeInfo {
+func appendInOrder(list []queryServer, item queryServer) []queryServer {
 	for i := range list {
-		if list[i].NodeID > item.NodeID {
+		if list[i].nodeInfo.NodeID > item.nodeInfo.NodeID {
 			if i == 0 {
-				return append([]service.NodeInfo{item}, list...)
+				return append([]queryServer{item}, list...)
 			}
 			return append(append(list[:i], item), list[i:]...)
 		}
@@ -324,7 +331,7 @@ func (m *ServiceMgr) GetCurrentTopology(rev service.Revision, cancel service.Can
 	if m.servers != nil && len(m.servers) != 0 {
 		checkPrepareOperations(m.servers, "ServiceMgr::GetCurrentTopology")
 		for _, s := range m.servers {
-			topology.Nodes = append(topology.Nodes, s.NodeID)
+			topology.Nodes = append(topology.Nodes, s.nodeInfo.NodeID)
 		}
 	} else {
 		topology.Nodes = append(topology.Nodes, m.nodeInfo.NodeID)
@@ -340,8 +347,7 @@ func (m *ServiceMgr) GetCurrentTopology(rev service.Revision, cancel service.Can
 	return topology, nil
 }
 
-func prepareOperation(node string, caller string) interface{} {
-	host := distributed.RemoteAccess().UUIDToHost(node)
+func prepareOperation(host string, caller string) interface{} {
 	ps, err := distributed.RemoteAccess().PrepareAdminOp(host, "shutdown", "", nil, distributed.NO_CREDS, "")
 	if err != nil {
 		logging.Debuga(func() string {
@@ -351,60 +357,62 @@ func prepareOperation(node string, caller string) interface{} {
 	return ps
 }
 
-func checkPrepareOperations(servers []service.NodeInfo, caller string) {
+func checkPrepareOperations(servers []queryServer, caller string) {
 	for i := range servers {
-		if servers[i].Opaque == nil {
-			servers[i].Opaque = prepareOperation(string(servers[i].NodeID), caller)
+		if servers[i].nodeInfo.Opaque == nil {
+			servers[i].nodeInfo.Opaque = prepareOperation(servers[i].host, caller)
 		}
 	}
 }
 
-const _PREPARE_RETRY_DELAY = 5 * time.Second
-
 // when preparing all we're doing is updating the cached nodes list from the list of retained nodes
 func (m *ServiceMgr) PrepareTopologyChange(change service.TopologyChange) error {
 	logging.Debuga(func() string { return fmt.Sprintf("ServiceMgr::PrepareTopologyChange entry: %v", change) })
-	defer logging.Debugf("ServiceMgr::PrepareTopologyChange exit")
 
 	if change.Type != service.TopologyChangeTypeFailover && change.Type != service.TopologyChangeTypeRebalance {
+		logging.Debugf("ServiceMgr::PrepareTopologyChange exit")
 		return service.ErrNotSupported
 	}
 
+	logging.Infof("Preparing for topology change")
+
 	// for each node we know about, cache its shutdown URL
 	info := make([]rune, 0, len(change.KeepNodes)*64)
-	servers := make([]service.NodeInfo, 0)
-	retry := false
+	servers := make([]queryServer, 0)
+	m.mu.Lock()
+	s := m.servers
+	m.mu.Unlock()
 	for _, n := range change.KeepNodes {
-		ps := prepareOperation(string(n.NodeInfo.NodeID), "ServiceMgr::PrepareTopologyChange")
-		if ps == nil {
-			retry = true
+		var ps interface{}
+		var host string
+		ps = nil
+		// see if we can reuse the prepared operation
+		// note: this means we may take less time here but are susceptible to stale data
+		for _, o := range s {
+			if o.nodeInfo.NodeID == n.NodeInfo.NodeID {
+				ps = o.nodeInfo.Opaque
+				host = o.host
+				break
+			}
 		}
-		servers = append(servers, service.NodeInfo{n.NodeInfo.NodeID, service.Priority(0), ps})
+		if ps == nil {
+			host = distributed.RemoteAccess().UUIDToHost(string(n.NodeInfo.NodeID))
+			ps = prepareOperation(host, "ServiceMgr::PrepareTopologyChange")
+		}
+		servers = append(servers, queryServer{host, service.NodeInfo{n.NodeInfo.NodeID, service.Priority(0), ps}})
 		info = append(info, []rune(n.NodeInfo.NodeID)...)
 		info = append(info, '[')
-		info = append(info, []rune(distributed.RemoteAccess().UUIDToHost(string(n.NodeInfo.NodeID)))...)
+		info = append(info, []rune(host)...)
 		info = append(info, ']')
 		info = append(info, ' ')
 	}
 
-	if retry {
-		// some failed.  This is likely a synchronisation issue where we've been invoked ahead of the cluster information
-		// changing.  This is a blunt delay before retrying any that have failed.  If they still fail, we'll try again whenever
-		// polled for the topologly and again prior to invocation.
-		time.Sleep(_PREPARE_RETRY_DELAY)
-		logging.Debuga(func() string { return "Retrying failed admin operation prepares" })
-		checkPrepareOperations(servers, "ServiceMgr::PrepareTopologyChange")
-	}
-
 	// always keep a local list of servers that are no longer present; only the master will ever act upon this list
-	var eject []service.NodeInfo
-	m.mu.Lock()
-	s := m.servers
-	m.mu.Unlock()
+	var eject []queryServer
 	for _, o := range s {
 		found := false
 		for _, n := range servers {
-			if o.NodeID == n.NodeID {
+			if o.nodeInfo.NodeID == n.nodeInfo.NodeID {
 				found = true
 				break
 			}
@@ -427,7 +435,8 @@ func (m *ServiceMgr) PrepareTopologyChange(change service.TopologyChange) error 
 	if len(info) == 0 {
 		info = append(info, []rune("no active nodes")...)
 	}
-	logging.Infof("Topology changed to: %s", string(info))
+	logging.Infof("Topology now: %s", string(info))
+	logging.Debugf("ServiceMgr::PrepareTopologyChange exit")
 	return nil
 }
 
@@ -436,7 +445,6 @@ func (m *ServiceMgr) PrepareTopologyChange(change service.TopologyChange) error 
 // This is only invoked on the master which is then responsible for initiating changes on other nodes
 func (m *ServiceMgr) StartTopologyChange(change service.TopologyChange) error {
 	logging.Debuga(func() string { return fmt.Sprintf("ServiceMgr::StartTopologyChange %v", change) })
-	defer logging.Debugf("ServiceMgr::StartTopologyChange exit")
 
 	timeout := time.Duration(0)
 	data := ""
@@ -449,43 +457,50 @@ func (m *ServiceMgr) StartTopologyChange(change service.TopologyChange) error {
 			s.changeID = ""
 			s.eject = nil
 		})
+		logging.Debugf("ServiceMgr::StartTopologyChange exit")
 		return nil // we're doing nothing so just return
 	case service.TopologyChangeTypeRebalance:
 	default:
+		logging.Debugf("ServiceMgr::StartTopologyChange exit")
 		return service.ErrNotSupported
 	}
 
 	m.mu.Lock()
 	if m.eject != nil {
 		info := make([]rune, 0, len(m.eject)*33)
-		eject := make([]service.NodeInfo, 0, len(m.eject))
+		eject := make([]queryServer, 0, len(m.eject))
 		done := util.WaitCount{}
 		mutex := &sync.Mutex{}
 		// in parallel in case some take time to reach
-		for _, e := range m.eject {
-			go func() {
-				if e.Opaque == nil {
+		for i := range m.eject {
+			go func(i int) {
+				if m.eject[i].nodeInfo.Opaque == nil {
 					// if we failed to prepare it before now, we could well be too late but try again anyway
-					e.Opaque = prepareOperation(string(e.NodeID), "ServiceMgr::StartTopologyChange")
+					m.eject[i].nodeInfo.Opaque = prepareOperation(m.eject[i].host, "ServiceMgr::StartTopologyChange")
 				}
-				_, err := distributed.RemoteAccess().ExecutePreparedAdminOp(e.Opaque, "POST", data, nil, distributed.NO_CREDS, "")
+				_, err := distributed.RemoteAccess().ExecutePreparedAdminOp(m.eject[i].nodeInfo.Opaque, "POST", data, nil,
+					distributed.NO_CREDS, "")
 				if err == nil {
 					mutex.Lock()
 					if eject != nil {
-						eject = append(eject, e)
-						info = append(info, []rune(e.NodeID)...)
+						eject = append(eject, m.eject[i])
+						info = append(info, []rune(m.eject[i].nodeInfo.NodeID)...)
+						info = append(info, '[')
+						info = append(info, []rune(m.eject[i].host)...)
+						info = append(info, ']')
 						info = append(info, ' ')
 					}
 					mutex.Unlock()
 					logging.Debuga(func() string {
-						return fmt.Sprintf("ServiceMgr::StartTopologyChange initiated shutdown down on '%s'", string(e.NodeID))
+						return fmt.Sprintf("ServiceMgr::StartTopologyChange initiated shutdown down on '%s'",
+							string(m.eject[i].nodeInfo.NodeID))
 					})
 				} else {
-					logging.Infof("ServiceMgr::StartTopologyChange failed to start shutdown on '%s' (op:%v): %v", string(e.NodeID),
-						e.Opaque, err)
+					logging.Infof("ServiceMgr::StartTopologyChange failed to start shutdown on '%s' (op:%v): %v",
+						string(m.eject[i].nodeInfo.NodeID), m.eject[i].nodeInfo.Opaque, err)
 				}
 				done.Incr()
-			}()
+			}(i)
 		}
 		// wait for completion
 		if !done.Until(int32(len(m.eject)), timeout) {
@@ -513,6 +528,7 @@ func (m *ServiceMgr) StartTopologyChange(change service.TopologyChange) error {
 	}
 	m.mu.Unlock()
 
+	logging.Debugf("ServiceMgr::StartTopologyChange exit")
 	return nil
 }
 
