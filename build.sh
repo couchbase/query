f195254f41e731d6d5854371c3b17ae51b765de7
sitaramv <sitaram.vemulapalli@couchbase.com>
2022-03-25 15:08:30 -0700

    improve standalone build for various branches
    Change-Id: Iab521e85dec6e350d633d47065b3e8156e202e66
    Reviewed-on: https://review.couchbase.org/c/query/+/172862
    Reviewed-by: Donald Haggart <donald.haggart@couchbase.com>
    Reviewed-by: Bingjie Miao <bingjie.miao@couchbase.com>
    Tested-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Well-Formed: Restriction Checker

--- build.sh
+++ build.sh
@@ -41,12 +41,146 @@ done
 
 set -- $args
 
+cbranch=`git rev-parse --abbrev-ref HEAD`
+defbranch="master"
+
+
+get_repo() {
+     local path=$1
+     local mcommit=$2
+     local subpath=$3
+     local scommit=$4
+
+     #echo "$path" "$mcommit" "$subpath" "$scommit"
+
+     cd $GOPATH/src/$path
+     abranch=`git branch | awk '{print $2}'`
+     if [[ $abranch != $mcommit ]]; then
+         git checkout $mcommit
+     fi
+     if [[ $subpath != "" ]]
+     then
+         if [[ ! -d $subpath ]]
+         then
+            url="https://"$path
+            git clone -b $mcommit $url $subpath
+         fi
+         (cd $subpath; git checkout $scommit)
+     fi
+     cd - >> /dev/null
+}
+
+get_path_subpath_commit() {
+    dirs=`echo $3 | tr "\/" "\n"`
+    declare -i l=0
+    for d in $dirs
+    do
+       subpath=$d
+       l+=1
+    done
+
+    if [[ $subpath == $1 ]]; then
+         subpath=""
+         path=$3
+    else
+         declare -i i=0
+         for d in $dirs
+         do
+            if [[ $i == 0 ]]; then
+                path=$d
+            elif [[ $i -ne $l-1 ]]; then
+                path=${path}"/"${d}
+            fi
+            i+=1
+         done
+    fi
+
+    if [[ $4 == "" ]]; then
+         commit=$2
+    else
+          versions=`echo $4 | tr "-" "\n"`
+          for v in $versions
+          do
+             commit=$v
+          done
+    fi
+
+    if [[ $subpath != "" ]]; then
+         mcommit=$2
+    else
+         mcommit=$commit
+    fi
+
+    get_repo "$path" "$mcommit" "$subpath" "$commit"
+
+}
+
+
+repo_by_gomod() {
+     local file=$1
+     local repo=$2
+     local branch=$3
+     local subbranch=$4
+
+     C=`grep replace $file | grep ../$repo | grep -v module | grep -v "${repo}-"`
+     if [[ $C != "" ]]; then
+           opath=`echo $C| awk '{print $2}'`
+            get_path_subpath_commit "$repo" "$branch" "$opath"  ""
+     else
+           grepo=$repo
+           if [[ $subbranch != "" ]]; then
+               grepo="${repo}/${subbranch}"
+           fi
+           C=`grep $grepo $file | grep -v module | grep -v replace | grep -v indirect| grep -v "${repo}-"`
+           if [[ $C == "" ]]; then
+               return
+           fi
+           gpath=`echo $C| awk '{print $1}'`
+           vers=`echo $C| awk '{print $2}'`
+           get_path_subpath_commit "$repo" "$branch" "$gpath"  "$vers"
+     fi
+}
+
+
+repo_setup() {
+    repo_by_gomod go.mod query $cbranch
+    repo_by_gomod go.mod query-ee $cbranch
+    repo_by_gomod go.mod indexing $cbranch
+    repo_by_gomod go.mod go-couchbase $cbranch
+    repo_by_gomod go.mod gomemcached $cbranch
+    repo_by_gomod go.mod cbauth $cbranch
+    repo_by_gomod go.mod godbc $cbranch
+    repo_by_gomod go.mod goutils $cbranch
+    repo_by_gomod go.mod go_json $cbranch
+    repo_by_gomod go.mod gometa $cbranch
+    repo_by_gomod go.mod eventing-ee $cbranch
+    repo_by_gomod go.mod n1fty $cbranch
+    repo_by_gomod go.mod cbgt $cbranch
+    repo_by_gomod go.mod cbft $cbranch
+    repo_by_gomod ../n1fty/go.mod bleve $defbranch
+    repo_by_gomod ../n1fty/go.mod bleve $defbranch "v2"
+    repo_by_gomod ../cbft/go.mod zapx $defbranch "v11"
+    repo_by_gomod ../cbft/go.mod zapx $defbranch "v12"
+    repo_by_gomod ../cbft/go.mod zapx $defbranch "v13"
+    repo_by_gomod ../cbft/go.mod zapx $defbranch "v14"
+    repo_by_gomod ../cbft/go.mod zapx $defbranch "v15"
+    repo_by_gomod go.mod gocbcore-transactions $defbranch
+    repo_by_gomod go.mod gocbcore $defbranch "v10"
+    repo_by_gomod ../cbgt/go.mod gocbcore $defbranch "v9"
+}
+
+
 DevStandaloneSetup() {
     # curl fix match manifest
        (cd ../../couchbasedeps/go-curl; git checkout 20161221-couchbase)
     # indexer generated files
-       if [[ (! -f ../indexing/secondary/protobuf/query/query.pb.go) && ( -f ~/devbld/query.pb.go ) ]]; then
-           cp ~/devbld/query.pb.go ../indexing/secondary/protobuf/query/query.pb.go
+       if [[ (! -f ../indexing/secondary/protobuf/query/query.pb.go) ]]; then
+           if [[ -f ~/devbld/query.pb.go ]]; then
+               cp ~/devbld/query.pb.go ../indexing/secondary/protobuf/query/query.pb.go
+           fi
+           if [[ -f ~/devbld/$cbranch.query.pb.go ]]; then
+               cp ~/devbld/$cbranch.query.pb.go ../indexing/secondary/protobuf/query/query.pb.go
+           fi
        fi
     # eventing-ee generated files
        if [[ ( ! -L $GOPATH/lib ) ]]; then
@@ -65,52 +199,8 @@ DevStandaloneSetup() {
        if [[ ! -f ../eventing-ee/evaluator/impl/gen/parser/global_config_schema.go ]]; then
            (cd ../eventing-ee/evaluator/impl/gen/convertschema; go run generate.go  ../../parser/global_config_schema.json GlobalConfigSchema ../parser/global_config_schema.go)
        fi
-    # gocbcore points to master; gocbcore/v9 points to 9.1.8
-       if [[ -d ../gocbcore/v9 ]]
-       then
-           cd ../gocbcore/v9
-           C=`git describe --tags|grep -c "v9.1.8"`
-           cd -
-       else
-           C=0
-       fi
-       if [[ $C -eq 0 ]]
-       then
-           (cd ..; rm -rf gocbcore/v9; git clone -b v9.1.8 https://github.com/couchbase/gocbcore.git gocbcore/v9)
-       fi
-    # gocbcore points to master; gocbcore/v10 points to 10.0.8
-       if [[ -d ../gocbcore/v10 ]]
-       then
-           cd ../gocbcore/v10
-           C=`git describe --tags|grep -c "v10.0.8"`
-           cd -
-       else
-           C=0
-       fi
-       if [[ $C -eq 0 ]]
-       then
-           (cd ..; rm -rf gocbcore/v10; git clone -b v10.0.8 https://github.com/couchbase/gocbcore.git gocbcore/v10)
-       fi
-    # bleve version
-       if [[ ! -d ../../blevesearch/bleve/v2 ]]; then
-           (cd ../../blevesearch; git clone -b v2.2.2 http://github.com/blevesearch/bleve.git bleve/v2)
-       fi
-    # zapx versions
-       if [[ ! -d ../../blevesearch/zapx/v11 ]]; then
-           (cd ../../blevesearch; git clone -b v11.3.1 http://github.com/blevesearch/zapx.git zapx/v11)
-       fi
-       if [[ ! -d ../../blevesearch/zapx/v12 ]]; then
-           (cd ../../blevesearch; git clone -b v12.3.1 http://github.com/blevesearch/zapx.git zapx/v12)
-       fi
-       if [[ ! -d ../../blevesearch/zapx/v13 ]]; then
-           (cd ../../blevesearch; git clone -b v13.3.1 http://github.com/blevesearch/zapx.git zapx/v13)
-       fi
-       if [[ ! -d ../../blevesearch/zapx/v14 ]]; then
-           (cd ../../blevesearch; git clone -b v14.3.1 http://github.com/blevesearch/zapx.git zapx/v14)
-       fi
-       if [[ ! -d ../../blevesearch/zapx/v15 ]]; then
-           (cd ../../blevesearch; git clone -b v15.3.1 http://github.com/blevesearch/zapx.git zapx/v15)
-       fi
+
+       repo_setup
        (cd $GOPATH/src/golang.org/x/net; git checkout `go version |  awk -F'[. ]' '{print "release-branch." $3 "." $4}'`)
 }
 
@@ -124,6 +214,8 @@ if [[ ( ! -d ../../../../../cbft && "$GOPATH" != "") || ( $sflag == 1) ]]; then
      go get $* $uflag -d -v ./...
      if [[ $sflag == 1 ]]; then
          DevStandaloneSetup
+         # go get again because repro branches might changed
+         go get $* $uflag -d -v ./...
      fi
 fi
 
