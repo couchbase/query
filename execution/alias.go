fe1434fa362b70985c6999ec6c1907ee9411fca2
Marco Greco <marco.greco@couchbase.com>
2022-07-28 18:20:44 +0100

    MB-53165 alias to account for alias memory usage
    Change-Id: I756b1d94df0ac385c993882ed5b4ff69e3d4d228
    Reviewed-on: https://review.couchbase.org/c/query/+/178127
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Tested-by: Marco Greco <marco.greco@couchbase.com>
    (cherry picked from commit 85e67fd0cd6aa081da6947980ef78d07c00487c3)
    Reviewed-on: https://review.couchbase.org/c/query/+/178140
    Well-Formed: Restriction Checker
    Tested-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>

--- execution/alias.go
+++ execution/alias.go
@@ -66,6 +66,16 @@ func (this *Alias) processItem(item value.AnnotatedValue, context *Context) bool
 	}
 	av.ShareAnnotations(item)
 	av.SetField(this.plan.Alias(), item)
+
+	// we should really use av.Size() - item.Size() but this is faster
+	if context.UseRequestQuota() {
+		err := context.TrackValueSize(uint64(len(this.plan.Alias())))
+		if err != nil {
+			context.Error(err)
+			av.Recycle()
+			return false
+		}
+	}
 	return this.sendItem(av)
 }
 
