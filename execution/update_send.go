63b698e21047461b2d0bbe32365886d473858dbd
dhaggart <donald.haggart@couchbase.com>
2022-12-07 08:35:40 +0000

    MB-54393 Fix build issues
    Change-Id: I7411fde124c1390351eeaec85fcf4574b64da854
    Reviewed-on: https://review.couchbase.org/c/query/+/183786
    Well-Formed: Restriction Checker
    Tested-by: Donald Haggart <donald.haggart@couchbase.com>
    Reviewed-by: Donald Haggart <donald.haggart@couchbase.com>

--- execution/update_send.go
+++ execution/update_send.go
@@ -228,8 +228,8 @@ func getExpiration(options value.Value) (exptime uint32) {
 const _MONTH = uint32(30 * 24 * 60 * 60)
 
 func adjustExpiration(options value.Value, copyOptions bool) value.Value {
-	expiration, present := getExpiration(options)
-	if present && expiration > 0 && expiration < _MONTH {
+	expiration := getExpiration(options)
+	if expiration > 0 && expiration < _MONTH {
 		expiration += uint32(time.Now().UTC().Unix())
 		if copyOptions {
 			options = options.CopyForUpdate()
