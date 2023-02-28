7ae8839d2e21170051d261a284d4864a0f0404d2
dhaggart <donald.haggart@couchbase.com>
2023-02-28 21:51:14 +0000

    Revise node-quota setting description
    Change-Id: I0b83c12ea61ed4c3071448aa18a409f470428a6a
    Reviewed-on: https://review.couchbase.org/c/query/+/187518
    Tested-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>

--- server/cbq-engine/main.go
+++ server/cbq-engine/main.go
@@ -122,7 +122,7 @@ var ENTERPRISE = flag.Bool("enterprise", true, "Enterprise mode")
 var MAX_INDEX_API = flag.Int("max-index-api", datastore_package.INDEX_API_MAX, "Max Index API")
 var N1QL_FEAT_CTRL = flag.Uint64("n1ql-feat-ctrl", util.DEF_N1QL_FEAT_CTRL, "N1QL Feature Controls")
 var MEMORY_QUOTA = flag.Uint64("memory-quota", _DEF_MEMORY_QUOTA, "Maximum amount of document memory allowed per request, in MB")
-var NODE_QUOTA = flag.Uint64("node-quota", _DEF_NODE_QUOTA, "Maximum amount of document memory allowed per node, in MB")
+var NODE_QUOTA = flag.Uint64("node-quota", _DEF_NODE_QUOTA, "Soft memory limit per node, in MB")
 var USE_REPLICA = flag.String("use-replica", value.TRISTATE_NAMES[value.NONE], "Allow reading from replica vBuckets")
 var NODE_QUOTA_VAL_PERCENT = flag.Uint("node-quota-val-percent", _DEF_NODE_QUOTA_VAL_PERCENT,
 	"Percentage of node quota reserved for value memory (0-100).")
