ee6b7fa1d75b2c3fcef20c0c87b6621090e4091d
miaobingjie <bingjie.miao@couchbase.com>
2023-02-28 20:59:53 -0700

    MB-55675 Fix test cases
    Change-Id: I1a25b39092d680690c72ac9dd36a0f200c139475
    Reviewed-on: https://review.couchbase.org/c/query/+/187532
    Tested-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>

--- test/gsi/test_cases/indexcbo/indexcbo_test.go
+++ test/gsi/test_cases/indexcbo/indexcbo_test.go
@@ -29,14 +29,14 @@ func TestIndexCBO(t *testing.T) {
 	fmt.Println("\n\nInserting values into Bucket for Indexing with CBO \n\n ")
 	runMatch("insert.json", false, false, qc, t)
 
-	runStmt(qc, "CREATE INDEX p_productId ON product(productId) WHERE test_id = \"indexCBO\"")
-	runStmt(qc, "CREATE INDEX p_productId_reviews ON product(productId, DISTINCT reviewList) WHERE test_id = \"indexCBO\"")
 	runStmt(qc, "CREATE INDEX iorix1 ON orders (c1, c2, c3) WHERE test_id = \"indexCBO\"")
 	runStmt(qc, "CREATE INDEX iorix2 ON orders (c1, c2, c4, c6) WHERE test_id = \"indexCBO\"")
+	runStmt(qc, "CREATE INDEX p_productId ON product(productId) WHERE test_id = \"indexCBO\"")
+	runStmt(qc, "CREATE INDEX p_productId_reviews ON product(productId, DISTINCT reviewList) WHERE test_id = \"indexCBO\"")
 
 	// run UPDATE STATISTICS statements
-	runStmt(qc, "UPDATE STATISTICS FOR product INDEX(p_productId_reviews)")
 	runStmt(qc, "UPDATE STATISTICS FOR orders INDEX(iorix1, iorix2)")
+	runStmt(qc, "UPDATE STATISTICS FOR product INDEX(p_productId_reviews)")
 
 	runMatch("case_intersect_scan.json", false, true, qc, t)
 
@@ -48,8 +48,8 @@ func TestIndexCBO(t *testing.T) {
 
 	runStmt(qc, "DROP INDEX product.p_productId")
 	runStmt(qc, "DROP INDEX product.p_productId_reviews")
-	runStmt(qc, "DROP INDEX purchase.iorix1")
-	runStmt(qc, "DROP INDEX purchase.iorix2")
+	runStmt(qc, "DROP INDEX orders.iorix1")
+	runStmt(qc, "DROP INDEX orders.iorix2")
 
 	runStmt(qc, "create primary index on product ")
 	runStmt(qc, "create primary index on purchase")
