0d551c4a0faedc1775adc6df2dc2e175dc7a6b92
dhaggart <donald.haggart@couchbase.com>
2023-03-07 15:26:37 +0000

    MB-57587 Track LET values
    LET values form part of the returned annotated value's size that is
    ultimately released so must be accounted for.  It is simpler to
    release the initial reservation and establish another once all bindings
    have been handled.
    
    Change-Id: I45cbd687a4733fdacb37a991f84f3d7a9b9049e5
    Reviewed-on: https://review.couchbase.org/c/query/+/187841
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Reviewed-by: Marco Greco <marco.greco@couchbase.com>
    Tested-by: Donald Haggart <donald.haggart@couchbase.com>
    (cherry picked from commit 992689c18b1f1281b2d6e8cd06797bbc8f4ec829)
    Reviewed-on: https://review.couchbase.org/c/query/+/192798
    Well-Formed: Restriction Checker
    Tested-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>

--- execution/let.go
+++ execution/let.go
@@ -50,7 +50,16 @@ func (this *Let) RunOnce(context *Context, parent value.Value) {
 }
 
 func (this *Let) processItem(item value.AnnotatedValue, context *Context) bool {
-	lv := item.Copy().(value.AnnotatedValue)
+	var lv value.AnnotatedValue
+	if context.UseRequestQuota() {
+		context.ReleaseValueSize(item.Size())
+	}
+	if item.RefCnt() == 1 {
+		lv = item
+	} else {
+		lv = item.CopyForUpdate().(value.AnnotatedValue)
+		item.Recycle()
+	}
 	for _, b := range this.plan.Bindings() {
 		v, e := b.Expression().Evaluate(lv, context)
 		if e != nil {
@@ -60,8 +69,11 @@ func (this *Let) processItem(item value.AnnotatedValue, context *Context) bool {
 
 		lv.SetField(b.Variable(), v)
 	}
-
-	item.Recycle()
+	if context.UseRequestQuota() && context.TrackValueSize(lv.Size()) {
+		context.Error(errors.NewMemoryQuotaExceededError())
+		lv.Recycle()
+		return false
+	}
 	return this.sendItem(lv)
 }
 
