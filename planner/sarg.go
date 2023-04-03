0be7054b50a7f05aaf093c67090fb9cea09ace83
miaobingjie <bingjie.miao@couchbase.com>
2023-04-03 17:02:34 -0600

    MB-56284 Detect and mark "constant" filters in subterms of OR-clause
    The issue was first identified in 7.0.5 (MB-53793) and not in
    later code lines since we handle OR-clause differently in later
    codelines. However, we've identified issues with OR-clause
    handling and fix the issues with MB-56039, after which the
    symptom of MB-53793 now shows in 7.2.0 and 7.5.0.
    
    The problematic query has an OR-clause like:
    
    (c2 = 6 AND IFMISSINGORNULL($qp1, "") != "") OR (IFMISSINGORNULL($qp1, "") == "")
    
    the issue is we have part of the predicate in each arm of the
    OR-clause as "constant" predicate, i.e. it does not depend on
    the document, rather it only depends on a named parameter.
    Such subclauses causes issues when sarging index keys since
    they don't generate any index spans but they are not detected
    by the "covering" test we currently use to check whether the
    index spans are "exact". The issue is somewhat similar to
    MB-50071 except the "constant" predicate in this case is part
    of subterms of an OR-clause and thus not detected by the fix
    for MB-50071.
    
    To fix this issue, we need to add a mechanism to detect such
    "constant" predicates in subterms of an OR-clause. This is
    built on top of the fix for MB-42474, and we check in case of
    OR-clause whether any subterm of the OR has such "contant"
    predicates, if so, mark an extra flag to prevent index
    aggregation pushdown.
    
    An extra modification is also done for classifying expressions,
    such that when we have an OR clause under and AND, we do the
    necessary OR-clause processing to remove any constant subterms.
    
    Change-Id: Icef39a8a0ca1f1a25e6dd62da62197b798e68159
    Reviewed-on: https://review.couchbase.org/c/query/+/189176
    Tested-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>

--- planner/sarg.go
+++ planner/sarg.go
@@ -59,14 +59,22 @@ func sargForOr(or *expression.Or, entry *indexEntry, keys expression.Expressions
 		spans[i] = s
 		exact = exact && ex
 
-		if exact && (max1 < max) && !entry.HasFlag(IE_OR_NON_SARG_EXPR) {
-			// check for non-sargable key in predicate
-			exprs, _, err := indexCoverExpressions(entry, keys[:max1], c, nil, baseKeyspace.Name(), context)
-			if err != nil {
-				return nil, false, err
+		if exact && !entry.HasFlag(IE_OR_NON_SARG_EXPR) {
+			setFlag := false
+			if max1 < max {
+				// check for non-sargable key in predicate
+				exprs, _, err := indexCoverExpressions(entry, keys[:max1], c, nil, baseKeyspace.Name(), context)
+				if err != nil {
+					return nil, false, err
+				}
+				implicitAny := implicitAnyCover(entry, true, context.FeatureControls())
+				if !expression.IsCovered(c, baseKeyspace.Name(), exprs, implicitAny) {
+					entry.SetFlags(IE_OR_NON_SARG_EXPR, true)
+					setFlag = true
+				}
+
 			}
-			implicitAny := implicitAnyCover(entry, true, context.FeatureControls())
-			if !expression.IsCovered(c, baseKeyspace.Name(), exprs, implicitAny) {
+			if !setFlag && hasConstSubExpr(c, keyspaceNames) {
 				entry.SetFlags(IE_OR_NON_SARG_EXPR, true)
 			}
 		}
@@ -334,4 +342,28 @@ func getSargSpans(pred expression.Expression, sargKeys expression.Expressions, i
 	}
 
 	return sargSpans, exactSpan, nil
+}
+
+// this function is called for subterms of an OR clause, it assumes the original OR expression
+// is already flattened, thus it does not expect an OR expression in here
+func hasConstSubExpr(pred expression.Expression, keyspaceNames map[string]string) bool {
+	if pred == nil || len(keyspaceNames) == 0 {
+		return false
+	}
+
+	if and, ok := pred.(*expression.And); ok {
+		for _, op := range and.Operands() {
+			if hasConstSubExpr(op, keyspaceNames) {
+				return true
+			}
+		}
+		return false
+	}
+
+	keyspaces, err := expression.CountKeySpaces(pred, keyspaceNames)
+	if err == nil && len(keyspaces) == 0 {
+		return true
+	}
+
+	return false
 }
