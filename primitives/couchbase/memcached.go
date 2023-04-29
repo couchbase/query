0c2812d64bb686cbde9bc24d9518fd434b89b97f
Marco Greco <marco.greco@couchbase.com>
2023-04-29 11:19:33 +0100

    MB-56680 check for more timeout errors
    Change-Id: If94b2cc154283e563fb095350510611324348647
    Reviewed-on: https://review.couchbase.org/c/query/+/190308
    Tested-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>

--- primitives/couchbase/memcached.go
+++ primitives/couchbase/memcached.go
@@ -629,7 +629,8 @@ func IsReadTimeOutError(err error) bool {
 	}
 	estr := err.Error()
 	return strings.Contains(estr, "read tcp") ||
-		strings.Contains(estr, "i/o timeout")
+		strings.Contains(estr, "i/o timeout") ||
+		strings.Contains(estr, "connection timed out")
 }
 
 func isTimeoutError(err error) bool {
