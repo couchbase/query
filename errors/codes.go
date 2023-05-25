bb01d5d1c039f8e356515a2673f2090c96bde0bd
Marco Greco <marco.greco@couchbase.com>
2023-05-25 08:31:43 +0100

    MB-57103 fix error codes
    Change-Id: Ic3e0c392fb920554fe7dd271617cdc6ca7c4a1f8
    Reviewed-on: https://review.couchbase.org/c/query/+/191561
    Reviewed-by: Donald Haggart <donald.haggart@couchbase.com>
    Tested-by: Marco Greco <marco.greco@couchbase.com>

--- errors/codes.go
+++ errors/codes.go
@@ -85,9 +85,9 @@ const (
 	E_SERVICE_TENANT_MISSING                  ErrorCode = 1197
 	E_SERVICE_TENANT_NOT_AUTHORIZED           ErrorCode = 1198
 	E_SERVICE_TENANT_REJECTED                 ErrorCode = 1199
-	E_SERVICE_TENANT_NOT_FOUND                ErrorCode = 1120
-	E_SERVICE_REQUEST_QUEUE_FULL              ErrorCode = 1121
-	E_SERVICE_NO_CLIENT                       ErrorCode = 1122
+	E_SERVICE_TENANT_NOT_FOUND                ErrorCode = 1200
+	E_SERVICE_REQUEST_QUEUE_FULL              ErrorCode = 1201
+	E_SERVICE_NO_CLIENT                       ErrorCode = 1202
 	E_ADMIN_CONNECTION                        ErrorCode = 2000
 	E_ADMIN_START                             ErrorCode = 2001
 	E_ADMIN_INVALIDURL                        ErrorCode = 2010
