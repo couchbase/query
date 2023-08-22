0cdf062ae1ae0af010948f2f4bab04f5afa77571
Donald Haggart <donald.haggart@couchbase.com>
2023-08-22 12:25:53 +0100

    Fix unit tests
    Change-Id: Ic9fc896831748b5388b6ba9f0b8262145ab048c4
    Reviewed-on: https://review.couchbase.org/c/query/+/195804
    Tested-by: Bingjie Miao <bingjie.miao@couchbase.com>
    Reviewed-by: Bingjie Miao <bingjie.miao@couchbase.com>

--- test/gsi/test_cases/sequential_scans/cs_test.go
+++ test/gsi/test_cases/sequential_scans/cs_test.go
@@ -22,12 +22,22 @@ func TestSequentialScans(t *testing.T) {
 
 	qc := start_cs()
 
+	// Tests require an empty bucket as we don't want to add the test_id filter to statements therein
+	rr := runStmt(qc, "DELETE FROM orders")
+	if rr.Err != nil {
+		t.Errorf("did not expect err %s", rr.Err.Error())
+	}
+	rr = runStmt(qc, "DELETE FROM orders._default.ss")
+	if rr.Err != nil {
+		t.Errorf("did not expect err %s", rr.Err.Error())
+	}
+
 	fmt.Println("\n\nInserting test data...\n\n ")
 	runMatch("insert.json", false, false, qc, t)
 
 	runMatch("case_ss.json", false, false, qc, t)
 
-	rr := runStmt(qc, "DELETE FROM orders WHERE test_id = \"ss\"")
+	rr = runStmt(qc, "DELETE FROM orders WHERE test_id = \"ss\"")
 	if rr.Err != nil {
 		t.Errorf("did not expect err %s", rr.Err.Error())
 	}
