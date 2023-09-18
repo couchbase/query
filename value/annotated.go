3426db060d663d0d03a6cc2df0cb35b8d2634176
dhanyagowrish <dhanya.gowrish@couchbase.com>
2023-09-18 15:43:01 +0530

    MB-58565 [7.1.5.MP4]: Ensure "constants" are not altered
    Change-Id: I154a731bb1bccde72d61e01e3e4d68caf3906a16
    Reviewed-on: https://review.couchbase.org/c/query/+/197308
    Well-Formed: Restriction Checker
    Tested-by: Bingjie Miao <bingjie.miao@couchbase.com>
    Reviewed-by: Bingjie Miao <bingjie.miao@couchbase.com>

--- value/annotated.go
+++ value/annotated.go
@@ -29,8 +29,11 @@ func init() {
 	util.NewLocklessPool(&annotatedPool, func() unsafe.Pointer {
 		return unsafe.Pointer(&annotatedValue{})
 	})
-	EMPTY_ANNOTATED_OBJECT = NewAnnotatedValue(map[string]interface{}{})
-	EMPTY_ANNOTATED_OBJECT.(*annotatedValue).noRecycle = true
+	av := newAnnotatedValue()
+	av.noRecycle = true
+	av.refCnt = 2 // should ensure it is copied and not directly modified by LET etc.
+	av.Value = NewValue(map[string]interface{}{})
+	EMPTY_ANNOTATED_OBJECT = av
 }
 
 func newAnnotatedValue() *annotatedValue {
@@ -474,7 +477,9 @@ func (this *annotatedValue) Seen() bool {
 }
 
 func (this *annotatedValue) Track() {
-	atomic.AddInt32(&this.refCnt, 1)
+	if !this.noRecycle {
+		atomic.AddInt32(&this.refCnt, 1)
+	}
 }
 
 func (this *annotatedValue) Recycle() {
@@ -487,10 +492,14 @@ func (this *annotatedValue) RefCnt() int32 {
 
 func (this *annotatedValue) recycle(lvl int32) {
 
+	// don't change the refCnt if marked as not-recycleable
+	if this.noRecycle {
+		return
+	}
 	// do no recycle if other scope values are using this value
 	// or if this is an original document hanging off a projecton
 	refcnt := atomic.AddInt32(&this.refCnt, lvl)
-	if refcnt > 0 || this.noRecycle {
+	if refcnt > 0 {
 		return
 	}
 	if refcnt < 0 {
