1fb2d0aeabf3533f35aef7ea8944d9dc0310e859
sitaramv <sitaram.vemulapalli@couchbase.com>
2023-10-17 17:09:53 -0700

    MB-59539 [7.2.3-MP1]: Add hints to text
    Change-Id: Ib32899ef40f9054463e1b594fb70a02489959bc3
    Reviewed-on: https://review.couchbase.org/c/query/+/198981
    Reviewed-by: Bingjie Miao <bingjie.miao@couchbase.com>
    Tested-by: Bingjie Miao <bingjie.miao@couchbase.com>
    Reviewed-on: https://review.couchbase.org/c/query/+/199026
    Reviewed-by: Donald Haggart <donald.haggart@couchbase.com>
    Tested-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Well-Formed: Restriction Checker
    Reviewed-on: https://review.couchbase.org/c/query/+/200625
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>

--- algebra/use.go
+++ algebra/use.go
@@ -57,3 +57,16 @@ func (this *Use) JoinHint() JoinHint {
 func (this *Use) SetJoinHint(joinHint JoinHint) {
 	this.joinHint = joinHint
 }
+
+func (this JoinHint) String() string {
+	s := ""
+	switch this {
+	case USE_HASH_BUILD:
+		s += " hash (build)"
+	case USE_HASH_PROBE:
+		s += " hash (probe)"
+	case USE_NL:
+		s += " nl"
+	}
+	return s
+}
