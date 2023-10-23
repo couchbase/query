6d237998c854acff4aedb699f4c34a726d546c03
Dhanya Gowrish <dhanya.gowrish@couchbase.com>
2023-10-23 15:05:08 +0000

    Revert "MB-59187: Add causing error to ICause for some E_EVALUATION_ABORT errors"
    This reverts commit ac5bc03103b70e200efc7a00a6a12c2d48e95ca8.
    
    Reason for revert: This change makes Evaluation errors un-readable in some cases
    
    Change-Id: Id7267bbe63995df8dae19ddf476e1615622323ef
    Reviewed-on: https://review.couchbase.org/c/query/+/198140
    Reviewed-by: Donald Haggart <donald.haggart@couchbase.com>
    Tested-by: Dhanya Gowrish <dhanya.gowrish@couchbase.com>

--- errors/execution.go
+++ errors/execution.go
@@ -46,7 +46,7 @@ func NewEvaluationError(e error, termType string) Error {
 		return &err{level: EXCEPTION, ICode: E_EVALUATION, IKey: "execution.abort_error", ICause: e,
 			InternalMsg: fmt.Sprintf("Abort: %s.", e), InternalCaller: CallerN(1)}
 	} else if ee, ok := e.(Error); ok {
-		return &err{level: EXCEPTION, ICode: E_EVALUATION_ABORT, IKey: "execution.evaluation_error", ICause: ee, cause: ee,
+		return &err{level: EXCEPTION, ICode: E_EVALUATION_ABORT, IKey: "execution.evaluation_error", cause: ee,
 			InternalMsg: fmt.Sprintf("Error evaluating %s", termType), InternalCaller: CallerN(1)}
 	}
 	return &err{level: EXCEPTION, ICode: E_EVALUATION_ABORT, IKey: "execution.evaluation_error", ICause: e,
@@ -58,7 +58,7 @@ func NewEvaluationWithCauseError(e error, termType string) Error {
 		return &err{level: EXCEPTION, ICode: E_EVALUATION, IKey: "execution.abort_error", ICause: e,
 			InternalMsg: fmt.Sprintf("Abort: %s.", e), InternalCaller: CallerN(1)}
 	} else if ee, ok := e.(Error); ok {
-		return &err{level: EXCEPTION, ICode: E_EVALUATION_ABORT, IKey: "execution.evaluation_error", ICause: ee, cause: ee,
+		return &err{level: EXCEPTION, ICode: E_EVALUATION_ABORT, IKey: "execution.evaluation_error", cause: ee,
 			InternalMsg: fmt.Sprintf("Error evaluating %s", termType), InternalCaller: CallerN(1)}
 	}
 	var c map[string]interface{}
