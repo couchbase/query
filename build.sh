6358e357cdc25c552932c35263f6e140ab8df8b7
sitaramv <sitaram.vemulapalli@couchbase.com>
2023-11-03 09:34:40 -0700

    MB-59530 7.1.x build
    Change-Id: I107109d53fc69283ed3b9e3ec15707af9efb8a48
    Reviewed-on: https://review.couchbase.org/c/query/+/200081
    Tested-by: Bingjie Miao <bingjie.miao@couchbase.com>
    Well-Formed: Restriction Checker
    Reviewed-by: Bingjie Miao <bingjie.miao@couchbase.com>

--- build.sh
+++ build.sh
@@ -48,92 +48,280 @@ done
 
 set -- $args
 
-DevStandaloneSetup() {
+GIT=`which git`
+
+cbranch=`$GIT rev-parse --abbrev-ref HEAD`
+rbranch=`$GIT log -n 5 --pretty=format:"%D"|awk 'NF>0{p=$NF}END{print p}'`
+defbranch="master"
+
+function checkout_if_necessary {
+  local current=`$GIT rev-parse --abbrev-ref HEAD 2>/dev/null`
+  local commit=`$GIT log -n 1 --pretty=format:"%h" 2>/dev/null`
+
+  if [[ -z $current ]]
+  then
+    # isn't a repo so can't check anything out
+    return
+  fi
+
+  local report=""
+  local res=""
+  # if there is no subpath passed in then we'll report errors else remain silent
+  report_errors=$1
+  shift
+
+  D=`echo ${PWD}|sed -E 's,.*github.com/couchbase/,,;s,.*golang.org,golang.org,'`
+  #echo "checkout_if_necessary: [${PWD}] ${D} -> $@"
+
+  while [[ $# > 0 ]]
+  do
+    branch=$1
+    shift
+    if [[ $branch == $current || $branch == $commit ]]
+    then
+      return
+    fi
+    cmd1="$GIT checkout $branch"
+    res=`$cmd1 2>&1`
+    if [[ $res =~ "did not match any file" ]]
+    then
+      # try refreshing the repo
+      ($GIT pull 2>/dev/null 1>/dev/null)  # no need to report status
+      res=`$cmd1 2>&1`
+    fi
+    if [[ ! $res =~ "is now at" ]]
+    then
+      report="${report}${D} -> ${cmd1}:\n${res}\n"
+    else
+      return  # success
+    fi
+  done
+  if [[ -z $report_errors && -n $report ]]
+  then
+    echo -e "$report"
+    echo
+  fi
+}
+
+function get_repo {
+    local path=$1
+    local mcommit=$2
+    local subpath=$3
+    local scommit=$4
+    shift
+    shift
+    shift
+    shift
+
+    #echo "get_repo: $path $mcommit $subpath $scommit $@"
+
+    if [ ! -d $GOPATH/src/$path ]
+    then
+        if [[ $path =~ "github.com" ]]
+        then
+            url="git@${path/\//:}.git"
+        else
+            url="https://${path}.git"
+        fi
+        $GIT clone -b $mcommit $url $GOPATH/src/$path
+    fi
+    cd $GOPATH/src/$path
+    checkout_if_necessary $subpath $mcommit $@
+    if [[ $subpath != "" ]]
+    then
+        if [[ ! -d $subpath ]]
+        then
+            if [[ $path =~ "github.com" ]]
+            then
+                url="git@${path/\//:}.git"
+            else
+                url="https://${path}.git"
+            fi
+            $GIT clone -b $mcommit $url $subpath
+        fi
+        (cd $subpath; checkout_if_necessary "" $scommit $@)
+    fi
+    cd - >> /dev/null
+}
+
+function get_path_subpath_commit {
+    #echo "get_path_subpath_commit: $@"
+    local repo=$1
+    local ipath=$2
+    local vers=$3
+    shift
+    shift
+    shift
+
+    subpath=`echo $ipath|awk -F/ '{print $NF}'`
+    if [[ $subpath == $repo ]]; then
+        subpath=""
+        path=$ipath
+    else
+        path=`echo $ipath|sed -E 's,/[^/]+$,,'`
+    fi
+
+    if [[ $vers == "" ]]; then
+         commit=$1
+    else
+         commit=`echo $vers|awk -F- '{print $NF}'`
+    fi
+
+    if [[ $subpath != "" ]]; then
+         mcommit=$1
+    else
+         mcommit=$commit
+    fi
+
+    get_repo "$path" "$mcommit" "$subpath" "$commit" $@
+
+}
+
+
+function repo_by_gomod {
+    #echo "repo_by_gomod: $@"
+    local file=$1
+    local repo=$2
+    local subbranch=$3
+    shift
+    shift
+    shift
+
+    C=`grep replace $file | grep "../${repo}" | grep -v "module" | grep -v "${repo}-"`
+    if [[ -n "$C" ]]
+    then
+        opath=`echo $C| awk '{print $2}'`
+        get_path_subpath_commit "$repo" "$opath" "" $@
+    else
+        grepo=$repo
+        if [[ $subbranch != "" ]]; then
+            grepo="${repo}/${subbranch}"
+        fi
+        C=`grep -w "${grepo}" $file | grep -v module | grep -v replace | grep -v indirect| grep -v "${repo}-"`
+        bpath=`echo $repo|awk -F/ '{print $1}'`
+        if [ -z "$C" ]; then
+            C=`grep -w "${grepo}" $file | grep -v module | grep -v replace | grep indirect| grep -v "${repo}-"`
+        fi
+        if [[ -z "$C" ]]
+        then
+            C="github.com/couchbase/${repo} $4"
+        fi
+        gpath=`echo $C| awk '{print $1}'`
+	vers=`echo $C| awk '{print $2}'|awk -F- '{print $NF}'`
+	if [[ $@ == "" ]]; then
+	     # use go.mod version
+             get_path_subpath_commit "$repo" "$gpath" "$vers" "" "$vers"
+	else
+             get_path_subpath_commit "$repo" "$gpath" "$vers" $@
+	fi
+    fi
+}
+
+
+function repo_setup {
+    repo_by_gomod go.mod query "" $cbranch $rbranch $defbranch
+    repo_by_gomod go.mod query-ee "" $cbranch $rbranch $defbranch
+    repo_by_gomod go.mod indexing "" $cbranch $rbranch $defbranch
+    repo_by_gomod go.mod go-couchbase "" $cbranch $rbranch $defbranch
+    repo_by_gomod go.mod gomemcached "" $cbranch $rbranch $defbranch
+    repo_by_gomod go.mod cbauth "" $cbranch $rbranch $defbranch
+    repo_by_gomod go.mod godbc "" $cbranch $rbranch $defbranch
+    repo_by_gomod go.mod goutils "" $cbranch $rbranch $defbranch
+    repo_by_gomod go.mod go_json "" $cbranch $rbranch $defbranch
+    repo_by_gomod go.mod gometa "" $cbranch $rbranch $defbranch
+    repo_by_gomod go.mod eventing-ee "" $cbranch $rbranch $defbranch
+    repo_by_gomod go.mod n1fty "" $cbranch $rbranch $defbranch
+    repo_by_gomod go.mod cbgt "" $cbranch $rbranch $defbranch
+    repo_by_gomod go.mod cbft "" $cbranch $rbranch $defbranch
+    repo_by_gomod go.mod hebrew "" $cbranch $rbranch $defbranch
+    repo_by_gomod ../n1fty/go.mod bleve "" $defbranch
+    repo_by_gomod ../n1fty/go.mod bleve "v2" $defbranch
+    repo_by_gomod ../cbft/go.mod zapx "v11" $defbranch
+    repo_by_gomod ../cbft/go.mod zapx "v12" $defbranch
+    repo_by_gomod ../cbft/go.mod zapx "v13" $defbranch
+    repo_by_gomod ../cbft/go.mod zapx "v14" $defbranch
+    repo_by_gomod ../cbft/go.mod zapx "v15" $defbranch
+    repo_by_gomod ../n1fty/go.mod blevesearch/geo "" $defbranch
+    repo_by_gomod ../n1fty/go.mod blevesearch/sear "" $defbranch
+    repo_by_gomod ../n1fty/go.mod blevesearch/bleve_index_api "" $defbranch
+    repo_by_gomod ../n1fty/go.mod blevesearch/scorch_segment_api "v2" $defbranch
+    repo_by_gomod ../n1fty/go.mod go.etcd.io/bbolt "" $defbranch
+    repo_by_gomod go.mod gocbcore "v10" $defbranch
+    repo_by_gomod ../n1fty/go.mod gocbcore "v9" $defbranch
+    repo_by_gomod go.mod x/net "" `go version |  awk -F'[. ]' '{print "release-branch." $3 "." $4}'` $defbranch
+    (cd ../godbc; git checkout 7c2b871f63b894e46627da5e5aa5fffb3a017878)
+    (cd ../hebrew; git checkout fd6a0a1013434e4ba8cae73f6c80d2433d72ed0b)
+    (cd ../blance; git checkout b2ec44a33677b3c64c516ed9e9c49721e266a58a)
+}
+
+function DevStandaloneSetup {
     # curl fix match manifest
        (cd ../../couchbasedeps/go-curl; git checkout 20161221-couchbase)
+
+    repo_setup
+
     # indexer generated files
-       if [[ (! -f ../indexing/secondary/protobuf/query/query.pb.go) && ( -f ~/devbld/query.pb.go ) ]]; then
-           cp ~/devbld/query.pb.go ../indexing/secondary/protobuf/query/query.pb.go
-       fi
+    if [[ -f ~/devbld/protoc-gen-go ]]
+    then
+            ln -sf ~/devbld/protoc-gen-go $GOPATH/bin
+	    (cd $GOPATH/src/github.com/couchbase/indexing/secondary/protobuf/query; protoc -I. --plugin=protoc-gen-go=$GOPATH/bin//protoc-gen-go query.proto --go_out=`pwd`)
+    fi
+
+    if [[ (! -f ../indexing/secondary/protobuf/query/query.pb.go) ]]
+    then
+        base=
+        if [[ -d ~/devbld ]]
+        then
+            base=~/devbld
+        elif [[ -d ~/code/devbld ]]
+        then
+            base=~/code/devbld
+        fi
+        if [[ -n "${base}" ]]
+        then
+            if [[ -f $base/$cbranch.query.pb.go ]]
+            then
+                cp $base/$cbranch.query.pb.go ../indexing/secondary/protobuf/query/query.pb.go
+            elif [[ -f $base/query.pb.go ]]
+            then
+                cp $base/query.pb.go ../indexing/secondary/protobuf/query/query.pb.go
+            fi
+        fi
+    fi
     # eventing-ee generated files
-       if [[ ( ! -L $GOPATH/lib ) ]]; then
-           if [[ -d $GOPATH/lib ]]
-           then
-             rm -rf $GOPATH/lib
-           fi
-           if [[ "Linux" = `uname` ]]
-           then
-             ln -s /opt/couchbase/lib $GOPATH/lib
-           elif [[ "Darwin" = `uname` ]]
-           then
-             ln -s "/Applications/Couchbase Server.app/Contents/Resources/couchbase-core/lib" $GOPATH/lib
-           fi
-       fi
-       if [[ ! -f ../eventing-ee/evaluator/impl/gen/parser/global_config_schema.go ]]; then
-           (cd ../eventing-ee/evaluator/impl/gen/convertschema; go run generate.go  ../../parser/global_config_schema.json GlobalConfigSchema ../parser/global_config_schema.go)
-       fi
-    # gocbcore points to master; gocbcore/v9 points to 9.1.8
-       if [[ -d ../gocbcore/v9 ]]
-       then
-           cd ../gocbcore/v9
-           C=`git describe --tags|grep -c "v9.1.8"`
-           cd -
-       else
-           C=0
-       fi
-       if [[ $C -eq 0 ]]
-       then
-           (cd ..; rm -rf gocbcore/v9; git clone -b v9.1.8 https://github.com/couchbase/gocbcore.git gocbcore/v9)
-       fi
-    # gocbcore points to master; gocbcore/v10 points to 10.0.8
-       if [[ -d ../gocbcore/v10 ]]
-       then
-           cd ../gocbcore/v10
-           C=`git describe --tags|grep -c "v10.0.8"`
-           cd -
-       else
-           C=0
-       fi
-       if [[ $C -eq 0 ]]
-       then
-           (cd ..; rm -rf gocbcore/v10; git clone -b v10.0.8 https://github.com/couchbase/gocbcore.git gocbcore/v10)
-       fi
-    # bleve version
-       if [[ ! -d ../../blevesearch/bleve/v2 ]]; then
-           (cd ../../blevesearch; git clone -b v2.3.3 http://github.com/blevesearch/bleve.git bleve/v2)
-       fi
-    # zapx versions
-       if [[ ! -d ../../blevesearch/zapx/v11 ]]; then
-           (cd ../../blevesearch; git clone -b v11.3.4 http://github.com/blevesearch/zapx.git zapx/v11)
-       fi
-       if [[ ! -d ../../blevesearch/zapx/v12 ]]; then
-           (cd ../../blevesearch; git clone -b v12.3.4 http://github.com/blevesearch/zapx.git zapx/v12)
-       fi
-       if [[ ! -d ../../blevesearch/zapx/v13 ]]; then
-           (cd ../../blevesearch; git clone -b v13.3.4 http://github.com/blevesearch/zapx.git zapx/v13)
-       fi
-       if [[ ! -d ../../blevesearch/zapx/v14 ]]; then
-           (cd ../../blevesearch; git clone -b v14.3.4 http://github.com/blevesearch/zapx.git zapx/v14)
-       fi
-       if [[ ! -d ../../blevesearch/zapx/v15 ]]; then
-           (cd ../../blevesearch; git clone -b v15.3.4 http://github.com/blevesearch/zapx.git zapx/v15)
-       fi
-       (cd $GOPATH/src/golang.org/x/net; git checkout `go version |  awk -F'[. ]' '{print "release-branch." $3 "." $4}'`)
+    if [[ ( ! -L $GOPATH/lib ) ]]; then
+    if [[ -d $GOPATH/lib ]]
+    then
+    rm -rf $GOPATH/lib
+    fi
+    if [[ "Linux" = `uname` ]]
+    then
+    ln -s /opt/couchbase/lib $GOPATH/lib
+    elif [[ "Darwin" = `uname` ]]
+    then
+    ln -s "/Applications/Couchbase Server.app/Contents/Resources/couchbase-core/lib" $GOPATH/lib
+    fi
+    fi
+    if [[ ! -f ../eventing-ee/evaluator/impl/gen/parser/global_config_schema.go ]]
+    then
+         (cd ../eventing-ee/evaluator/impl/gen/convertschema; go run generate.go  ../../parser/global_config_schema.json GlobalConfigSchema ../parser/global_config_schema.go)
+    fi
 }
 
 # turn off go module for non repo sync build or standalone build
-if [[ ( ! -d ../../../../../cbft && "$GOPATH" != "") || ( $sflag == 1) ]]; then
-     export GO111MODULE=off
-     export CGO_CFLAGS="-I$GOPATH/src/github.com/couchbase/eventing-ee/evaluator/worker/include $CGO_FLAGS"
-     export CGO_LDFLAGS="-L$GOPATH/lib $CGO_LDFLAGS"
-     export LD_LIBRARY_PATH=$GOPATH/lib:${LD_LIBRARY_PATH}
-     echo go get $* $uflag -d -v ./...
-     go get $* $uflag -d -v ./...
-     if [[ $sflag == 1 ]]; then
-         DevStandaloneSetup
-     fi
+if [[ ( ! -d ../../../../../cbft && "$GOPATH" != "") || ( $sflag != 0) ]]; then
+    export GO111MODULE=off
+    export CGO_CFLAGS="-I$GOPATH/src/github.com/couchbase/eventing-ee/evaluator/worker/include"
+    export CGO_LDFLAGS="-L$GOPATH/lib $CGO_LDFLAGS"
+    export LD_LIBRARY_PATH=$GOPATH/lib:${LD_LIBRARY_PATH}
+    cmd="go get $* $uflag -d -v ./..."
+    echo $cmd
+    $cmd
+    if [[ $sflag == 1 ]]; then
+        DevStandaloneSetup
+        $cmd
+    fi
 fi
-
 
 echo cd parser/n1ql
 cd parser/n1ql
