ffec643db3d0a9b09dd50b13ff6cb488a250533d
Bingjie Miao <bingjie.miao@couchbase.com>
2023-12-08 11:58:40 -0700

    MB-52176 Avoid gathering optimizer statistics when index already exists
    In Trinity we added the ability to automatically gather optimizer statistics
    when a (non-deferred) CREATE INDEX/CREATE PRIMARY INDEX statement is run,
    if CBO is turned on.
    
    If the index already exists and 'IF NOT EXISTS' option is specified, then
    we ignore the error for "index already exists", in which case we should not
    attempt to gather optimizer statistics.
    
    Change-Id: I926ee6edadd16b1b3e027dd6dfd6b2ad896fae79
    Reviewed-on: https://review.couchbase.org/c/query/+/202384
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Tested-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>

--- execution/index_create.go
+++ execution/index_create.go
@@ -112,8 +112,7 @@ func (this *CreateIndex) RunOnce(context *Context, parent value.Value) {
 					context.Error(err)
 					return
 				}
-			}
-			if context.useCBO && (node.Using() == datastore.GSI || node.Using() == datastore.DEFAULT) && !deferred(node.With()) {
+			} else if context.useCBO && (node.Using() == datastore.GSI || node.Using() == datastore.DEFAULT) && !deferred(node.With()) {
 				err = updateStats([]string{node.Name()}, "create_index", this.plan.Keyspace(), context)
 				if err != nil {
 					context.Error(err)
