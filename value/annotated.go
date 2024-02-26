9a7f92e95690e9fee14e5408e426a89f95f81e98
Bingjie Miao <bingjie.miao@couchbase.com>
2024-02-26 13:42:34 -0700

    MB-60942 Revert previous changes for MB-60762
    There are issues identified with the previous fixes for MB-60762,
    thus for 7.6.0 the previous fixes are now backed out. The behavior
    of MERGE statement now remains the same as in 7.2.x line.
    
    Revert "MB-60762 Copy over META information if inside transaction"
    
    This reverts commit e508179a405edb73b0fd4f8685a0b2538f2773e6
    
    Revert "MB-60762 Proper detection of duplication error in MERGE statement"
    
    This reverts commit 616b555ac9b4df61d7e7601a79be00c6b3e9dd29.
    
    Change-Id: Ifa0d575df4ec3047af615f09eae056cafc622e71
    Reviewed-on: https://review.couchbase.org/c/query/+/206337
    Well-Formed: Restriction Checker
    Tested-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>

--- value/annotated.go
+++ value/annotated.go
@@ -88,7 +88,6 @@ type AnnotatedValue interface {
 	SetId(id interface{})
 	NewMeta() map[string]interface{}
 	GetMeta() map[string]interface{}
-	SetMeta(meta map[string]interface{})
 	ResetMeta()
 	Covers() Value
 	GetCover(key string) Value
@@ -330,14 +329,6 @@ func (this *annotatedValue) GetMeta() map[string]interface{} {
 		this.meta = make(map[string]interface{}, _DEFAULT_ATTACHMENT_SIZE)
 	}
 	return this.meta
-}
-
-func (this *annotatedValue) SetMeta(meta map[string]interface{}) {
-	k := this.GetId()
-	this.meta = meta
-	if k != nil {
-		this.SetId(k)
-	}
 }
 
 func (this *annotatedValue) ResetMeta() {
@@ -1005,10 +996,6 @@ func (this *annotatedValueSelfReference) NewMeta() map[string]interface{} {
 
 func (this *annotatedValueSelfReference) GetMeta() map[string]interface{} {
 	return (*annotatedValue)(this).GetMeta()
-}
-
-func (this *annotatedValueSelfReference) SetMeta(meta map[string]interface{}) {
-	(*annotatedValue)(this).SetMeta(meta)
 }
 
 func (this *annotatedValueSelfReference) ResetMeta() {
