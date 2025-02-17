76df0259f3a6118ab7e20d37dac092e89a8b357c
Bingjie Miao <bingjie.miao@couchbase.com>
2025-02-16 20:16:58 -0700

    Unit test update
    The unit test indexcbo starts to fail for me after the latest
    changes for include columns. The reason being for whatever
    reason the newly created indexes are not memory resident and
    thus UPDATE STATISTICS did not properly gather index statistics
    for the new indexes, resulting in failure in CBO costing.
    
    Add a few queries to use the newly created indexes such that
    the indexes are at least partially in memory before running
    UPDATE STATISTICS.
    
    Change-Id: I53743bee6a7ec407a351cd78de68e0b279f4e454
    Reviewed-on: https://review.couchbase.org/c/query/+/223517
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Tested-by: Bingjie Miao <bingjie.miao@couchbase.com>

--- test/gsi/test_cases/indexcbo/indexcbo_test.go
+++ test/gsi/test_cases/indexcbo/indexcbo_test.go
@@ -34,6 +34,9 @@ func TestIndexCBO(t *testing.T) {
 	runStmt(qc, "CREATE INDEX p_productId ON product(productId) WHERE test_id = \"indexCBO\"")
 	runStmt(qc, "CREATE INDEX p_productId_reviews ON product(productId, DISTINCT reviewList) WHERE test_id = \"indexCBO\"")
 
+	// run a few queries to make sure indexes are in memory before UPDATE STATISTICS
+	runMatch("case_use_index.json", false, false, qc, t)
+
 	// run UPDATE STATISTICS statements
 	runStmt(qc, "UPDATE STATISTICS FOR orders INDEX(iorix1, iorix2)")
 	runStmt(qc, "UPDATE STATISTICS FOR product INDEX(p_productId_reviews)")
