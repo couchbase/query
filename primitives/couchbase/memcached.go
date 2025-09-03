ab0016594e48972a4ebe247bc00c551afdf212c8
dhanyagowrish <dhanya.gowrish@couchbase.com>
2025-09-03 12:56:20 +0530

    MB-68354: Fix incorrect read count in bucket diagnostic information
    - The bulk get operation performs the read count increment with the the
      size of the structure containing the fetched documents.
    
    - Previously, this operation was performed after returning the
      structure to the pool. This caused incorrect increments, as the
      structure could have been re-used from the pool by another bulk
      get request.
    
    - Fix this by performing the read count increment before returning the
      response structure to the pool.
    
    Change-Id: I1924fba462cc354261009af33f13caef6d98dd84
    Reviewed-on: https://review.couchbase.org/c/query/+/233194
    Tested-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Well-Formed: Restriction Checker
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>

--- primitives/couchbase/memcached.go
+++ primitives/couchbase/memcached.go
@@ -1022,8 +1022,8 @@ func vbDoBulkGet(vbg *vbBulkGet) time.Duration {
 		}
 	}
 	if len(rv) > 0 {
-		vbg.ch <- rv
 		atomic.AddUint64(&vbg.b.readCount, uint64(len(rv)))
+		vbg.ch <- rv
 	}
 	return delay
 }
