06f51a1b808e9cb5abdb7327d2cfc052c0b8d5d6
Sohoxic <soham.sarkar@couchbase.com>
2025-12-10 00:44:48 +0530

    MB-69218: Merge remote-tracking branch 'origin/trinity' into morpheus
    Change-Id: Ib5f1b3b4315b0e6befc7d9e5daef1d1c4ae8bfb1
    Reviewed-on: https://review.couchbase.org/c/query/+/237447
    Well-Formed: Restriction Checker
    Tested-by: SOHAM SARKAR <soham.sarkar@couchbase.com>
    Reviewed-by: SOHAM SARKAR <soham.sarkar@couchbase.com>

--- inferencer/describe_keyspace.go
+++ inferencer/describe_keyspace.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"os"
 	"runtime/pprof"
+	"runtime"
 	"strconv"
 	"strings"
 	"time"
@@ -281,7 +282,16 @@ func (di *DefaultInferencer) InferKeyspace(context datastore.QueryContext, ks da
 	docCount, _ := ks.Count(context)
 
 	defer close(conn.ValueChannel())
-
+	defer func() {
+		if r := recover(); r != nil {
+			buf := make([]byte, 1<<16)
+			n := runtime.Stack(buf, false)
+			s := string(buf[0:n])
+			logging.Severef("panic in InferKeyspace: %v", r)
+			logging.Severef("stack: %v", s)
+			conn.Error(errors.NewInferKeyspaceError(ks.Name(), fmt.Errorf("panic: %v", r)))
+		}
+	}()
 	options, err := processWith(context, with)
 	if err != nil {
 		conn.Error(err)
@@ -332,7 +342,16 @@ func (di *DefaultInferencer) InferExpression(context datastore.QueryContext, exp
 	conn *datastore.ValueConnection) {
 
 	defer close(conn.ValueChannel())
-
+	defer func() {
+		if r := recover(); r != nil {
+			buf := make([]byte, 1<<16)
+			n := runtime.Stack(buf, false)
+			s := string(buf[0:n])
+			logging.Severef("panic in InferExpression: %v", r)
+			logging.Severef("stack: %v", s)
+			conn.Error(errors.NewInferExpressionEvalFailed(fmt.Errorf("panic: %v", r)))
+		}
+	}()
 	options, err := processWith(context, with)
 	if err != nil {
 		conn.Error(err)
