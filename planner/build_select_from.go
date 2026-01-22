32fb1f21c07a474e3df8660c102d3067ac7d592c
Dhanya Gowrish <dhanya.gowrish@couchbase.com>
2026-01-22 07:20:36 +0000

    Revert "MB-69725 Merge remote-tracking branch 'origin/trinity' into morpheus"
    This reverts commit 591e6e8d66925cd7d7fad23cb331ded3fd9afb90.
    
    Reason for revert: An issue MB-70258 noticed as a consequence of these changes.
    
    Change-Id: Ibcff6e2c1b24e37642785e639153c18509bd70a7
    Reviewed-on: https://review.couchbase.org/c/query/+/239004
    Tested-by: Dhanya Gowrish <dhanya.gowrish@couchbase.com>
    Well-Formed: Restriction Checker
    Reviewed-by: Dhanya Gowrish <dhanya.gowrish@couchbase.com>

--- planner/build_select_from.go
+++ planner/build_select_from.go
@@ -1188,7 +1188,7 @@ func offsetPlusLimit(offset, limit expression.Expression) expression.Expression 
 			limit = expression.NewGreatest(expression.ZERO_EXPR, limit)
 		}
 
-		return expression.NewAdd(limit.Copy(), offset.Copy())
+		return expression.NewAdd(limit, offset)
 	} else {
 		return limit
 	}
