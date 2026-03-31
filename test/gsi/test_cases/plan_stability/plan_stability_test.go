1937fd3d202f749e34072af9a9605abba0fc7373
Bingjie Miao <bingjie.miao@couchbase.com>
2026-03-30 18:26:34 -0600

    MB-65809 Add unit test
    Initial version of unit test for plan stability. Only covers the basic
    operations, and only cover plan stability mode thus far. Will add more
    test cases for error policy, subquery support etc. at a later date.
    
    Change-Id: I1ad5d70bb118b71f85313a935b25f320a87cd2b3
    Reviewed-on: https://review.couchbase.org/c/query/+/242557
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Tested-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>

---
+++ test/gsi/test_cases/plan_stability/plan_stability_test.go
@@ -0,0 +1,55 @@
+//  Copyright 2026-Present Couchbase, Inc.
+//
+//  Use of this software is governed by the Business Source License included
+//  in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+//  in that file, in accordance with the Business Source License, use of this
+//  software will be governed by the Apache License, Version 2.0, included in
+//  the file licenses/APL2.txt.
+
+package plan_stability
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+)
+
+// Basic test on plan stability
+func TestPlanStability(t *testing.T) {
+	if strings.ToLower(os.Getenv("GSI_TEST")) != "true" {
+		return
+	}
+
+	qc := start_cs()
+
+	fmt.Print("\n\nInserting values into Buckets for plan stability\n\n")
+	runMatch("insert.json", false, false, qc, t)
+
+	fmt.Println("Creating indexes")
+	runStmt(qc, "CREATE INDEX st_idx1 on shellTest(c11, c12) WHERE type = \"left\"")
+	runStmt(qc, "CREATE INDEX st_idx2 on shellTest(c21, c22) WHERE type = \"right\"")
+
+	fmt.Println("Running Plan Stability test cases")
+
+	runMatch("case_plan_stability_start.json", false, false, qc, t)
+
+	runMatch("case_plan_stability_simple.json", false, true, qc, t)
+
+	//runMatch("case_plan_stability_subquery.json", false, true, qc, t)
+
+	runMatch("case_plan_stability_stop.json", false, false, qc, t)
+
+	fmt.Println("Dropping indexes")
+	runStmt(qc, "DROP INDEX shellTest.st_idx1")
+	runStmt(qc, "DROP INDEX shellTest.st_idx2")
+
+	// create primary indexes
+	runStmt(qc, "CREATE PRIMARY INDEX ON shellTest")
+
+	// delete all rows from keyspaces used
+	runStmt(qc, "DELETE FROM shellTest")
+
+	// drop primary indexes
+	runStmt(qc, "DROP PRIMARY INDEX ON shellTest")
+}
