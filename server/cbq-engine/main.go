d7a0f5a8cca04cad68815298ceec7261232ce99e
Bingjie Miao <bingjie.miao@couchbase.com>
2026-08-04 14:26:14 -0600

    MB-73140 Put migration in previous location in server startup
    The previous checkin incorrectly put the migration functions right after
    the listener, instead of its original location close to the end of the
    main routine. Put the migration functions to (almost) the prior location,
    the only difference is now it is put in front of the call to prime the
    prepared cache, which makes more logical sense since the verification of
    query plans may need to access CBO stats (e.g. for reprepare).
    
    Change-Id: I0bd168f8486f0412f811e41319b67c15d7a987f1
    Reviewed-on: https://review.couchbase.org/c/query/+/250052
    Tested-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Well-Formed: Restriction Checker
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>

--- server/cbq-engine/main.go
+++ server/cbq-engine/main.go
@@ -441,10 +441,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	// migrations (functions storage and CBO stats)
-	storage.Migrate()
-	server_package.MigrateDictionary()
-
 	er = endpoint.SetupSSL()
 	if er != nil {
 		logging.Errorf("Error with Setting up SSL endpoints : %v", err.Error())
@@ -456,6 +452,10 @@ func main() {
 
 	// topology awareness - after listeners are ready to handle requests
 	_ = control.NewManager(*UUID)
+
+	// migrations (functions storage and CBO stats)
+	storage.Migrate()
+	server_package.MigrateDictionary()
 
 	// Now that we are up and running, try to prime the prepareds cache
 	prepareds.PreparedsRemotePrime()
