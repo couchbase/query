bd68c7534add76053a6bd3c0c12512c96420f660
Bingjie Miao <bingjie.miao@couchbase.com>
2026-09-09 17:28:15 -0600

    MB-73846 Avoid index name collision in unit test
    Change-Id: I87d8a6223618303446fd8d37c61356b4eb6ac19b
    Reviewed-on: https://review.couchbase.org/c/query/+/253016
    Tested-by: Bingjie Miao <bingjie.miao@couchbase.com>
    Well-Formed: Restriction Checker
    Reviewed-by: Bingjie Miao <bingjie.miao@couchbase.com>

--- test/gsi/test_cases/indexscan/indexscan_test.go
+++ test/gsi/test_cases/indexscan/indexscan_test.go
@@ -191,7 +191,7 @@ func TestIndexScan(t *testing.T) {
 	runStmt(qc, "CREATE INDEX ishix4 ON shellTest(c5, c8, c10) WHERE c6 != 1 AND c7 NOT IN [1,2] AND c11 != 0")
 	runStmt(qc, "CREATE INDEX ishix5 ON shellTest(id) WHERE type = \"type1\"")
 	runStmt(qc, "CREATE INDEX ishix6 ON shellTest(id, type) WHERE type NOT IN [\"type2\",\"type3\"]")
-	runStmt(qc, "CREATE INDEX ishix7 ON shellTest(c11, c12, arr1, arr2)")
+	runStmt(qc, "CREATE INDEX ishix9 ON shellTest(c11, c12, arr1, arr2)")
 
 	runMatch("case_index_scan_bugs.json", false, true, qc, t)
 
@@ -205,7 +205,7 @@ func TestIndexScan(t *testing.T) {
 	runStmt(qc, "DROP INDEX shellTest.ishix4")
 	runStmt(qc, "DROP INDEX shellTest.ishix5")
 	runStmt(qc, "DROP INDEX shellTest.ishix6")
-	runStmt(qc, "DROP INDEX shellTest.ishix7")
+	runStmt(qc, "DROP INDEX shellTest.ishix9")
 
 	runStmt(qc, "create primary index on product ")
 	runStmt(qc, "create primary index on purchase")
